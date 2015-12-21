057d6849965b951fd1d2d8658f28c821ec10fc2a
Danping YAO <Thoughtworks@Anlihuer.local>
2015-12-21 19:09:20 +0800

    Add blank in app.js

--- app/app.js
+++ app/app.js
@@ -9,9 +9,13 @@ global.apiServer = 'http://localhost:8080/api/';
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
 app.use(bodyParser.json());
+
 app.use(express.static('public'));
+
 route.setRoutes(app);
+
 app.listen(3000, function() {
   console.log('App listening at http://localhost:3000');
 });
