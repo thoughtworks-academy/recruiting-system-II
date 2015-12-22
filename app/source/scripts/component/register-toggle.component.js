1574c8e050ad8def91fb9c02b6e65136e63b46d3
wengjiaojiao <419909058@qq.com>
2015-12-22 13:46:24 +0800

    Use react rewrite the demo about toggle

--- app/source/scripts/component/register-toggle.component.js
+++ app/source/scripts/component/register-toggle.component.js
@@ -1,7 +1,15 @@
 var React = require('react');
 var registerToggle = React.createClass({
-   render: function () {
 
+   toggleState: function() {
+      this.props.onStateChange();
+   },
+
+   render: function () {
+      return (
+          <li className="toggle" onClick={this.toggleState}>
+             {this.props.isShowToggle ? '隐藏密码' : '显示密码'}</li>
+      )
    }
 });
 
