0a80ae4c594a0005dc8494f3fa6f670842109749
wengjiaojiao <419909058@qq.com>
2015-12-22 09:17:32 +0800

    Remove the login js to register

--- app/source/scripts/register.js
+++ app/source/scripts/register.js
@@ -124,4 +124,31 @@ $(function() {
       register();
     }
   });
+  $('[name=loginFailed]').hide();
+
+  function jumpToStart() {
+    location.href = "start.html"
+  }
+
+  $("#login-btn").on('click', function (evt) {
+    var phoneEmail = $('[name=phoneEmail]').val();
+    var loginPassword = $('[name=loginPassword]').val();
+
+    request.get('/login')
+        .set('Content-Type', "application/json")
+        .query({
+          account: phoneEmail,
+          password: loginPassword
+        })
+        .end(function (err, req) {
+          var data = JSON.parse(req.text);
+          if (data.status === 200) {
+            jumpToStart();
+          } else {
+            $('[name=loginFailed]').show();
+            evt.preventDefault();
+          }
+        })
+  });
+
 });
