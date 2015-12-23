32d9fd2a14ec296670900d1f4c7418a1d0738051
wengjiaojiao <419909058@qq.com>
2015-12-23 10:36:54 +0800

    Extract the password element

--- app/source/scripts/component/register-form.component.js
+++ app/source/scripts/component/register-form.component.js
@@ -3,25 +3,7 @@ var $ = require('jquery');
 var ReactDOM = require('react-dom');
 var request = require('superagent');
 var validate = require("validate.js");
-var RegisterToggle = require('./register-toggle.component');
-
-
-function passwordSafe(val){
-    if (val == '') return 0;
-    var strongRegex = new RegExp('^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$', 'g');
-    var mediumRegex = new RegExp('^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$', 'g');
-    var enoughRegex = new RegExp('(?=.{6,}).*', 'g');
-    if (enoughRegex.test(val) == false) {
-        return 1;
-    } else if (strongRegex.test(val)) {
-        return 3;
-    } else if (mediumRegex.test(val)) {
-        return 2;
-    } else {
-        return 1;
-    }
-    return false;
-}
+var RegisterPassword = require('./register-password.component');
 
 var containers = {
     email: {
@@ -33,14 +15,6 @@ var containers = {
         format: {
             pattern: /^1[3|4|5|8][0-9]\d{8}$/,
             message: '^请输入合法手机号'
-        }
-    },
-    password: {
-        presence: {message: "^请输入密码"},
-        length: {
-            minimum: 8,
-            maximum: 16,
-            message: "^请输入合法密码"
         }
     }
 };
@@ -91,17 +65,8 @@ var RegisterForm = React.createClass({
         getInitialState: function () {
             return {
                 mobilePhoneError: '',
-                emailError: '',
-                passwordError: '',
-                passwordSafeLevel: '',
-                passwordSafeStyle: '',
-                isShowToggle: false
+                emailError: ''
             }
-        },
-
-        stateChange: function() {
-            var newState = !this.state.isShowToggle;
-            this.setState({isShowToggle: newState});
         },
 
         validate: function (event) {
@@ -123,24 +88,6 @@ var RegisterForm = React.createClass({
                     this.setState(stateObj);
                 })
             }
-        },
-
-        checkPasswordSafe: function(event){
-            var level = passwordSafe(event.target.value);
-            switch (level) {
-                case 1:
-                    this.setState({passwordSafeStyle: 'danger'});
-                    break;
-                case 2:
-                    this.setState({passwordSafeStyle: 'general'});
-                    break;
-                case 3:
-                    this.setState({passwordSafeStyle: 'safe'});
-                    break;
-                case 0:
-                    break;
-            }
-            this.setState({passwordSafeLevel: level});
         },
 
         register: function(){
@@ -196,18 +143,9 @@ var RegisterForm = React.createClass({
                             <div
                                 className={"lose" + (this.state.emailError === '' ? ' hide' : '')}>{this.state.emailError}</div>
                         </div>
+
                         <div className="form-group">
-                            <input className="form-control" type={(this.state.isShowToggle === false ? "password" : "text")} placeholder="请输入8~16位密码" name="password" ref="password"
-                                   id="register-password" onBlur={this.validate} onChange={this.checkPasswordSafe}/>
-                            <div
-                                className={"lose" + (this.state.passwordError === '' ? ' hide' : '')}>{this.state.passwordError}
-                            </div>
-                            <ul className="passport-safely">
-                                <li className={this.state.passwordSafeLevel >= 1 ? this.state.passwordSafeStyle : ""}>弱</li>&nbsp;
-                                <li className={this.state.passwordSafeLevel >= 2 ? this.state.passwordSafeStyle : ""}>中</li>&nbsp;
-                                <li className={this.state.passwordSafeLevel == 3 ? this.state.passwordSafeStyle : ""}>强</li>&nbsp;
-                                <RegisterToggle isShowToggle={this.state.isShowToggle} onStateChange={this.stateChange}/>
-                            </ul>
+                            <RegisterPassword />
                         </div>
 
                         <div className="checkbox">
