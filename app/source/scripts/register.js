a5bdc79f60a51b34aa7d0d1e3ab0b2307c8ab9eb
wengjiaojiao <419909058@qq.com>
2015-12-23 17:21:00 +0800

    Use react to rewrite the jquery demo about login quest

--- app/source/scripts/register.js
+++ app/source/scripts/register.js
@@ -11,36 +11,9 @@ ReactDom.render(
 );
 
 $(function() {
-
-  function jumpToStart() {
-    location.href = 'start.html'
-  }
-
   $('#agreementModal').on('show.bs.modal', function() {
     $('.modal .modal-body').css('overflow-y', 'auto').css('max-height', $(window).height() * 0.7);
   });
 
   $('[name=loginFailed]').hide();
-
-  $("#login-btn").on('click', function (evt) {
-    var phoneEmail = $('[name=phoneEmail]').val();
-    var loginPassword = $('[name=loginPassword]').val();
-
-    request.get('/login')
-        .set('Content-Type', "application/json")
-        .query({
-          account: phoneEmail,
-          password: loginPassword
-        })
-        .end(function (err, req) {
-          var data = JSON.parse(req.text);
-          if (data.status === 200) {
-            jumpToStart();
-          } else {
-            $('[name=loginFailed]').show();
-            evt.preventDefault();
-          }
-        })
-  });
-
 });
