1044dcf13cbc018a8d29f034b3d170552ed65d4b
Wenjun LIN <linwenjun@163.com>
2015-12-25 18:45:48 +0800

    TEST: user Junit 4 refactory paper test

--- api/src/test/java/com/thoughtworks/twars/mapper/PaperMapperTest.java
+++ api/src/test/java/com/thoughtworks/twars/mapper/PaperMapperTest.java
@@ -2,32 +2,35 @@ package com.thoughtworks.twars.mapper;
 
 import com.thoughtworks.twars.bean.Paper;
 import com.thoughtworks.twars.util.DBUtil;
-import junit.framework.TestCase;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class PaperMapperTest extends TestCase {
+public class PaperMapperTest {
 
-    private SqlSessionFactory sqlSessionFactory;
     private PaperMapper paperMapper;
-    private SqlSession session;
 
+    @Before
     public void setUp() throws Exception {
-        session = DBUtil.getSession();
+        SqlSession session = DBUtil.getSession();
         paperMapper = session.getMapper(PaperMapper.class);
     }
 
+    @After
     public void tearDown() throws Exception {
 
     }
 
+    @Test
     public void testFindAll() throws Exception {
         List<Paper> papers = paperMapper.findAll();
         assertThat(papers.size(), is(2));
+        assertThat(papers.get(0).getId(), is(1));
     }
 }
