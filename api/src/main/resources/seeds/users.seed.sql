ba3d9d20d03b3195b6b4eb6394a57b93ab2a6ed0
Wenjun LIN <linwenjun@163.com>
2015-12-26 20:05:17 +0800

    SQL: Add users.seed

---
+++ api/src/main/resources/seeds/users.seed.sql
@@ -0,0 +1,3 @@
+TRUNCATE users;
+INSERT INTO users values(null, "test@163.com", "12345678901", "simple_word", 1451117203);
+INSERT INTO users values(null, "test2@qq.com", "12345678902", "simple_word", 1451117300);
