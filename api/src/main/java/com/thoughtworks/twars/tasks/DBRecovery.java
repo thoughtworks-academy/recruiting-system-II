1453f218a52f619030d9cf00320a3692bd5d3252
Wenjun LIN <linwenjun@163.com>
2015-12-27 00:14:43 +0800

    Gradle: Add DBRecovery task

---
+++ api/src/main/java/com/thoughtworks/twars/tasks/DBRecovery.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.twars.tasks;
+
+import com.thoughtworks.twars.util.DBUtil;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.apache.ibatis.session.SqlSession;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Connection;
+
+public class DBRecovery {
+
+    public static void main(String[] args) throws IOException {
+        SqlSession session = DBUtil.getSession();
+        Connection connection = session.getConnection();
+        ScriptRunner runner = new ScriptRunner(connection);
+
+        Reader reader = Resources.getResourceAsReader("./seeds/master.sql");
+        runner.runScript(reader);
+    }
+}
