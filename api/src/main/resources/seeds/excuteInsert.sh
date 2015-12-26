5efe634da7bc82115059ce9c8f47408fdb173933
Wenjun LIN <linwenjun@163.com>
2015-12-27 00:12:51 +0800

    SQL Script: Move all sql statement into one file

--- api/src/main/resources/seeds/excuteInsert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mysql -u BronzeSword -p12345678 -D BronzeSword < ./quize-items.seed.sql
-mysql -u BronzeSword -p12345678 -D BronzeSword < ./paper.seed.sql
--- api/src/main/resources/seeds/paper.seed.sql
+++
@@ -1,3 +0,0 @@
-TRUNCATE paper;
-INSERT INTO paper values(null, 1);
-INSERT INTO paper values(null, 5);
