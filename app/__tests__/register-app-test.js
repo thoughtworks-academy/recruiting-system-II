f5adbe46449e8d1f1eed3e4190f4d5aea6ee5713
wengjiaojiao <419909058@qq.com>
2015-12-28 22:30:17 +0800

    Use jest to test about register-app

--- app/__tests__/register-app-test.js
+++ app/__tests__/register-app-test.js
@@ -1,17 +1,17 @@
 //jest.dontMock('../source/scripts/component/register-app.component.js');
 
-//import React from 'react';
-//import ReactDom from 'react-dom';
-//import TestUtils from 'react-addons-test-utils';
+var React = require('react');
+var ReactDom = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
 
-//const registerApp = require('../source/scripts/component/register-app.component.js');
 
 describe('registerApp', () => {
   it('change the info after click', () => {
-//    var registerLabel = TestUtils.renderIntoDocument(<registerApp isLoginState="false"/>);
-//    var registerNode = ReactDom.findDOMNode(registerLabel);
-//
-//    console.log(registerNode);
+    var registerApp = require('../source/scripts/component/register-app.component.js');
+
+    //var registerLabel = TestUtils.renderIntoDocument(<registerApp isLoginState="false"/>);
+    //var registerNode = ReactDom.findDOMNode(registerLabel);
+    //console.log(registerNode);
 //    console.log(1111111);
 
     //expect(registerNode).toEqual();
--- app/source/scripts/component/register-app.component.js
+++ app/source/scripts/component/register-app.component.js
@@ -1,33 +1,32 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-
 var RegisterForm = require('./register-form.component');
 var LoginForm = require('./login-form.component');
 var LoginInfo = require('./login-info.component');
 var RegisterAgreement = require('./register-agreement.component');
 
+//require("babel-core").transform("code", options);
 var RegisterApp = React.createClass({
 
-    getInitialState: function() {
+    getInitialState:function () {
         return {
             isLoginState: false
         }
     },
-
-    stateChange: function() {
+    stateChange:function () {
         var newState = !this.state.isLoginState;
         this.setState({isLoginState: newState});
     },
 
-    render: function() {
+    render() {
         return (
             <div className="row">
-                <RegisterForm isLoginState={this.state.isLoginState}/>
+                <RegisterForm isLoginState={this.state.isLoginState} />
                 <LoginForm isLoginState={this.state.isLoginState} />
                 <LoginInfo isLoginState={this.state.isLoginState} onStateChange={this.stateChange} />
                 <RegisterAgreement />
             </div>
-        )
+        );
     }
 });
 
