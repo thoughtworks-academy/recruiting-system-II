f73f27d1af33230de42e38f7b3105668be818aad
wengjiaojiao <419909058@qq.com>
2015-12-28 14:33:32 +0800

    Create jest test dir and add package

---
+++ app/__tests__/register-app-test.js
@@ -0,0 +1,23 @@
+//jest.dontMock('../source/scripts/component/register-app.component.js');
+
+//import React from 'react';
+//import ReactDom from 'react-dom';
+//import TestUtils from 'react-addons-test-utils';
+
+//const registerApp = require('../source/scripts/component/register-app.component.js');
+
+describe('registerApp', () => {
+  it('change the info after click', () => {
+//    var registerLabel = TestUtils.renderIntoDocument(<registerApp isLoginState="false"/>);
+//    var registerNode = ReactDom.findDOMNode(registerLabel);
+//
+//    console.log(registerNode);
+//    console.log(1111111);
+
+    //expect(registerNode).toEqual();
+    expect('a').toBe('a');
+  });
+});
+
+
+
