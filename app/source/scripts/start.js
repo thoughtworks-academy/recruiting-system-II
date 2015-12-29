22222cc4056d81505fe4f0fc3d83e60aff2b2138
wengjiaojiao <419909058@qq.com>
2015-12-29 14:01:32 +0800

    Less:add a cursor

--- app/source/scripts/start.js
+++ app/source/scripts/start.js
@@ -2,6 +2,7 @@ var $ = global.jQuery = require('jquery');
 var ReactDom = require('react-dom');
 var StartInfo = require('./component/start-info.component');
 require('bootstrap');
+require('../less/start.less');
 
 ReactDom.render(
     <StartInfo />,
