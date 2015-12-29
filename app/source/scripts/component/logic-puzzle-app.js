feb69dd073e6d07de340acef9a6861a798b0dcdc
Evan-Leee <EvanLee_@outlook.com>
2015-12-29 14:21:38 +0800

    APP : add getUserAnswer when the page loading

--- app/source/scripts/component/logic-puzzle-app.js
+++ app/source/scripts/component/logic-puzzle-app.js
@@ -24,6 +24,7 @@ var LogicPuzzle = React.createClass({
 
   componentDidMount: function () {
     LogicPuzzleActions.loadItem();
+    LogicPuzzleActions.getUserAnswer();
   },
 
   render: function () {
