5466951bb1ab146ca9255911c1a4998f8bc148eb
Wenjun LIN <linwenjun@163.com>
2015-12-29 17:44:53 +0800

    Gulp: Remove all task for gulp

--- app/gulp/tasks/watch-less.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-
-gulp.task('watch-less', function() {
-  livereload.listen();
-  gulp.watch(['./source/less/*.less'], ['less'])
-});
