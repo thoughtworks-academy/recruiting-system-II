806bfdbe4214213f957cb6424dfc1755cb73f86a
XTWang <meeting_love_lengyi@hotmail.com>
2015-12-30 15:02:24 +0800

    APP: Rewrite the function

--- app/routes/routers/user-puzzle.js
+++ app/routes/routers/user-puzzle.js
@@ -6,48 +6,55 @@ var constant = require('../../mixin/back-constant.json');
 
 
 router.post('/', function (req, res) {
+  var userId = req.session.user.id;
 
-  var userId = req.session.user.id || 5;
+  userPuzzle.findOne({userId: userId})
 
-  userPuzzle.findOne({userId: userId}, function (err, doc) {
+    .then(function(doc){
+      if(doc === null){
+        return new userPuzzle({
+          userId: userId,
+          startTime: Date.parse(new Date()) / 1000
+        });
+      }else {
+        throw new Error('isExist');
+      }
+    })
+    .then(function(newUser){
+      newUser.quizItems = [
+        {id: 1, uri : 'quizItems/1',  userAnswer: 9},
+        {id: 5, uri : 'quizItems/5',  userAnswer: null},
+        {id: 8, uri : 'quizItems/8',  userAnswer: null},
+        {id: 14, uri : 'quizItems/14',  userAnswer: 1},
+        {id: 26, uri : 'quizItems/26',  userAnswer: null},
+        {id: 33, uri : 'quizItems/33',  userAnswer: null},
+        {id: 36, uri : 'quizItems/36',  userAnswer: null},
+        {id: 41, uri : 'quizItems/41',  userAnswer: 1},
+        {id: 47, uri : 'quizItems/47',  userAnswer: null},
+        {id: 49, uri : 'quizItems/49',  userAnswer: null}
+      ];
 
-    if (err === null && doc === null) {
-
-      var newUser = new userPuzzle({
-        userId: userId,
-        startTime: Date.parse(new Date()) / 1000,
-        quizItems: [
-          {id: 1, uri : 'quizItems/1',  userAnswer: 9},
-          {id: 5, uri : 'quizItems/5',  userAnswer: null},
-          {id: 8, uri : 'quizItems/8',  userAnswer: null},
-          {id: 14, uri : 'quizItems/14',  userAnswer: 1},
-          {id: 26, uri : 'quizItems/26',  userAnswer: null},
-          {id: 33, uri : 'quizItems/33',  userAnswer: null},
-          {id: 36, uri : 'quizItems/36',  userAnswer: null},
-          {id: 41, uri : 'quizItems/41',  userAnswer: 1},
-          {id: 47, uri : 'quizItems/47',  userAnswer: null},
-          {id: 49, uri : 'quizItems/49',  userAnswer: null}
-        ]
-      });
-
-      newUser.save(function (err) {
-        if (err) {
-          res.send(err);
-          return console.error(err);
-        }else {
-          res.send({
-            status: constant.SUCCESSFUL_STATUS
-          });
-        }
-      });
-
-    }else {
-      res.send({
-        status: constant.SUCCESSFUL_STATUS
-      });
-    }
-
-  });
+      return newUser.save();
+    })
+    .then(function(doc, error){
+      if (error) {
+        res.send(error);
+      }else {
+        res.send({
+          status: constant.SUCCESSFUL_STATUS
+        });
+      }
+    })
+    .then(null, function (err) {
+      console.log(err.message);
+      if(err.message === 'isExist'){
+        res.send({
+          status: constant.SUCCESSFUL_STATUS
+        })
+      }else{
+        res.send(err);
+      }
+    });
 });
 
 
