892d2e05e39be40d708eb2662c5eb5cb0f701fcb
wengjiaojiao <419909058@qq.com>
2015-12-30 12:23:15 +0800

    JS:add passport and github_client_id

--- app/app.js
+++ app/app.js
@@ -10,9 +10,38 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var dbConnection = require('./middleware/db-connection');
 var sessionChick= require('./middleware/session-check');
+var passport = require('passport');
+var util = require('util');
+var GitHubStrategy = require('passport-github').Strategy;
+
+var GITHUB_CLIENT_ID = "3d1ce4b21c72eed40be3";
+var GITHUB_CLIENT_SECRET = "fe406b1fdc3f386871979976e244e01224c933ac";
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/login/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    }
+));
 
 app.use(cookieParser());
 app.use(session({secret: 'RECRUITING_SYSTEM', resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 global.apiServer = 'http://localhost:8080/api/';
 
