374d3a089c750202a9180421d053cf729edf1b89
XTWang <meeting_love_lengyi@hotmail.com>
2015-12-31 15:47:41 +0800

    App: remove the useless function

--- app/routes/routers/index.js
+++ app/routes/routers/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  console.log('');
-  res.sendFile(__dirname + '/public/index.html');
-});
+router.get('/');
 
 module.exports = router;
