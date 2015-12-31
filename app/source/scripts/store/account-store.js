518b6056c2f3fc662453e551fee73edefff817e6
wengjiaojiao <419909058@qq.com>
2015-12-31 15:48:26 +0800

    JS:use reflux to write the get request in the store

--- app/source/scripts/store/account-store.js
+++ app/source/scripts/store/account-store.js
@@ -1,3 +1,27 @@
-/**
- * Created by Jessica on 12/31/15.
- */
+var Reflux = require('reflux');
+var AccountActions = require('../actions/account-actions');
+var request = require('superagent');
+
+var AccountStore = Reflux.createStore({
+  listenables: [AccountActions],
+
+  onLoadUserInfo: function () {
+    request.get('/account')
+        .set('Content-Type', 'application/json')
+        .end((err, res) => {
+          this.trigger({
+            id: res.body.id,
+            school: res.body.school,
+            name: res.body.name,
+            mobilePhone: res.body.mobilePhone,
+            email: res.body.email,
+            gender: res.body.gender,
+            major: res.body.major,
+            grade: res.body.grade,
+            birthday: res.body.birth
+          });
+        })
+  }
+});
+
+module.exports = AccountStore;
