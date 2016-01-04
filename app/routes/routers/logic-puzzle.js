56c1ac16ea91f103739ac312c7adc136995889c2
sialvsic <sialvsic@outlook.com>
2016-01-04 09:21:10 +0800

    App: create userinfo before load userPuzzle

--- app/routes/routers/logic-puzzle.js
+++ app/routes/routers/logic-puzzle.js
@@ -15,8 +15,37 @@ router.get('/', function (req, res) {
 
   userPuzzle.findOne({userId: userId})
       .then(function (data) {
-        quizAll = data.quizDemos.concat(data.quizItems);
+        if (data === null) {
+          var newUser = new userPuzzle({
+            userId: userId
+          });
 
+          newUser.quizItems = [
+            {id: 1, uri: 'quizItems/1', userAnswer: 9},
+            {id: 5, uri: 'quizItems/5', userAnswer: null},
+            {id: 8, uri: 'quizItems/8', userAnswer: null},
+            {id: 14, uri: 'quizItems/14', userAnswer: 1},
+            {id: 26, uri: 'quizItems/26', userAnswer: null},
+            {id: 33, uri: 'quizItems/33', userAnswer: null},
+            {id: 36, uri: 'quizItems/36', userAnswer: null},
+            {id: 41, uri: 'quizItems/41', userAnswer: 1},
+            {id: 47, uri: 'quizItems/47', userAnswer: null},
+            {id: 49, uri: 'quizItems/49', userAnswer: null}
+          ];
+
+          newUser.quizDemos = [
+            {id: 2, uri: 'quizItems/2'},
+            {id: 3, uri: 'quizItems/3'},
+            {id: 4, uri: 'quizItems/4'}
+          ];
+
+          newUser.save(function () {
+            return 'success'
+          });
+        }
+
+      }).then(function (data) {
+        quizAll = data.quizDemos.concat(data.quizItems);
         itemsCount = quizAll.length;
         return quizAll[orderId].uri;
       })
