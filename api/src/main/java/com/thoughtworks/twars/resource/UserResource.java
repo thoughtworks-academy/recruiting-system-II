081fe60dea7a8b59b9d9326c1377f54c1891467d
Wenjun LIN <linwenjun@163.com>
2016-01-04 13:50:17 +0800

    API: Add api for get user detail

--- api/src/main/java/com/thoughtworks/twars/resource/UserResource.java
+++ api/src/main/java/com/thoughtworks/twars/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.twars.resource;
 
 import com.thoughtworks.twars.bean.User;
+import com.thoughtworks.twars.bean.UserDetail;
 import com.thoughtworks.twars.mapper.UserMapper;
 
 import javax.inject.Inject;
@@ -27,6 +28,23 @@ public class UserResource extends Resource {
         map.put("id", user.getId());
         map.put("email", user.getEmail());
         map.put("mobilePhone", user.getMobilePhone());
+
+        return Response.status(200).entity(map).build();
+    }
+
+    @GET
+    @Path("/{param}/detail")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUserDetail(@PathParam("param") int userId) {
+
+        UserDetail detail = userMapper.getUserDetailById(userId);
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", detail.getUserId());
+        map.put("school", detail.getSchool());
+        map.put("major", detail.getMajor());
+        map.put("degree", detail.getDegree());
+        map.put("name", detail.getName());
+        map.put("gender", detail.getGender());
 
         return Response.status(200).entity(map).build();
     }
--- api/src/test/java/com/thoughtworks/twars/resource/UserResourceTest.java
+++ api/src/test/java/com/thoughtworks/twars/resource/UserResourceTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.twars.resource;
 
 import com.thoughtworks.twars.bean.User;
+import com.thoughtworks.twars.bean.UserDetail;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
@@ -63,5 +64,39 @@ public class UserResourceTest extends TestBase {
                 .request().get();
 
         assertThat(response.getStatus(), is(404));
+    }
+
+    @Test
+    public void should_return_user_detail_by_user_id() throws Exception {
+
+        UserDetail theDetail = mock(UserDetail.class);
+
+        when(userMapper.getUserDetailById(1)).thenReturn(theDetail);
+        when(theDetail.getUserId()).thenReturn(1);
+        when(theDetail.getSchool()).thenReturn("哈佛");
+        when(theDetail.getMajor()).thenReturn("宗教");
+        when(theDetail.getDegree()).thenReturn("博士");
+        when(theDetail.getName()).thenReturn("狗剩");
+        when(theDetail.getGender()).thenReturn("男");
+
+
+
+
+
+
+        Response response = target(basePath + "/1/detail").request().get();
+
+        assertThat(response.getStatus(), is(200));
+
+        Map result = response.readEntity(Map.class);
+
+        assertThat(result.get("userId"), is(1));
+        assertThat(result.get("school"), is("哈佛"));
+        assertThat(result.get("major"), is("宗教"));
+        assertThat(result.get("degree"), is("博士"));
+        assertThat(result.get("name"), is("狗剩"));
+        assertThat(result.get("gender"), is("男"));
+//        assertThat(result.get("birthday"), is(0));
+
     }
 }
