913bcbf9555a77a898057e107eb00e7dacb943d1
sialvsic <sialvsic@outlook.com>
2016-01-05 16:09:32 +0800

    App: Add dashboard

--- app/source/scripts/component/dashboard.component.js
+++ app/source/scripts/component/dashboard.component.js
@@ -1,13 +1,61 @@
 var React = global.React = require('react');
 var request = require('superagent');
+var Grid = require('react-bootstrap/lib/Grid');
+var Row = require('react-bootstrap/lib/Row');
+var Col = require('react-bootstrap/lib/Col');
+var Image = require('react-bootstrap/lib/Image');
+var Button = require('react-bootstrap/lib/Button');
+var Thumbnail = require('react-bootstrap/lib/Thumbnail');
 
 var Dashboard = React.createClass({
 
 
   render() {
     return (
-        <div>
+        <div className="app-list">
+          <Row>
+            <Col xs={12} sm={6} md={4} lg={4}>
+              <a href="account.html" className="icon-view">
+                <div className="icon-wrapper">
+                  <div className="icon-img">
+                    <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+                  </div>
+                  <div className="icon-name">
+                    个人中心
+                  </div>
+                </div>
+
+              </a>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={4}>
+              <a href="logic-puzzle.html" className="icon-view">
+                <div className="icon-wrapper">
+                  <div className="icon-img">
+                    <span className="glyphicon glyphicon-education" aria-hidden="true"></span>
+                  </div>
+                  <div className="icon-name">
+                    逻辑题
+                  </div>
+                </div>
+
+              </a>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={4}>
+              <a href="dojo.html" className="icon-view">
+                <div className="icon-wrapper">
+                  <div className="icon-img">
+                    <span className="glyphicon glyphicon-road" aria-hidden="true"></span>
+                  </div>
+                  <div className="icon-name">
+                    编程题
+                  </div>
+                </div>
+
+              </a>
+            </Col>
+          </Row>
         </div>
+
     );
   }
 });
--- app/source/scripts/dashboard.js
+++ app/source/scripts/dashboard.js
@@ -2,8 +2,6 @@ var ReactDom = require('react-dom');
 var DashBoard = require('./component/dashBoard.component');
 require('../less/dashboard.less');
 
-
-
 ReactDom.render(
     <DashBoard />,
     document.getElementById('dashboard-container')
