1607a430d0a92c29c6ec4eac6e8e283359a0ca1f
sialvsic <sialvsic@outlook.com>
2016-01-05 12:24:53 +0800

    App: init dashboard.component

---
+++ app/source/scripts/component/dashboard.component.js
@@ -0,0 +1,15 @@
+var React = global.React = require('react');
+var request = require('superagent');
+
+var Dashboard = React.createClass({
+
+
+  render() {
+    return (
+        <div>
+        </div>
+    );
+  }
+});
+
+module.exports = Dashboard;
--- app/source/scripts/dashboard.js
+++ app/source/scripts/dashboard.js
@@ -1,2 +1,10 @@
 var ReactDom = require('react-dom');
+var DashBoard = require('./component/dashBoard.component');
 require('../less/dashboard.less');
+
+
+
+ReactDom.render(
+    <DashBoard />,
+    document.getElementById('dashboard-container')
+);
