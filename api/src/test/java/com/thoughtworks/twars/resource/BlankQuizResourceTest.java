0ea157ae18edb5e6cf914b9ab9961e9c43f83c5a
purpletianjing <purpletianjing@163.com>
2016-01-05 13:40:59 +0800

    delete test findByBlankQuizId

--- api/src/test/java/com/thoughtworks/twars/resource/BlankQuizResourceTest.java
+++ api/src/test/java/com/thoughtworks/twars/resource/BlankQuizResourceTest.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.twars.resource;
 import com.thoughtworks.twars.bean.BlankQuiz;
-import com.thoughtworks.twars.bean.QuizItem;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
@@ -9,9 +8,9 @@ import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.junit.Assert.assertThat;
 
 
 public class BlankQuizResourceTest extends TestBase{
@@ -56,30 +55,5 @@ public class BlankQuizResourceTest extends TestBase{
         assertThat(blankQuizzes.get(0).get("hardCount"),is(3));
         assertThat(blankQuizzes.get(0).get("normalCount"),is(4));
         assertThat(blankQuizzes.get(0).get("easyCount"),is(3));
-    }
-
-
-    @Test
-    public void should_return_quiz_items_by_blank_quiz_id(){
-        QuizItem firstQuizItem = mock(QuizItem.class);
-        QuizItem secondQuizItem = mock(QuizItem.class);
-
-        when(quizItemMapper.findByBlankQuizId(1)).thenReturn(Arrays.asList(firstQuizItem,secondQuizItem));
-        when(firstQuizItem.getId()).thenReturn(2);
-        when(firstQuizItem.getQuestionZh()).thenReturn("请输出6号格子的值:");
-        when(firstQuizItem.getDescriptionZh()).thenReturn("将4号盒子中的数字放在4号盒子中");
-        when(firstQuizItem.getChartPath()).thenReturn("/chart.jpg");
-        when(firstQuizItem.getInitializedBox()).thenReturn("2,3,5,7,4,2,4");
-
-        Response response = target(basePath+"/1").request().get();
-        assertThat(response.getStatus(),is(200));
-
-        List<Map> quizItems = response.readEntity(List.class);
-
-        assertThat(quizItems.get(0).get("id"),is(2));
-        assertThat(quizItems.get(0).get("question"),is("请输出6号格子的值:"));
-        assertThat(quizItems.get(0).get("description"),is("将4号盒子中的数字放在4号盒子中"));
-        assertThat(quizItems.get(0).get("initializedBox"),is("2,3,5,7,4,2,4"));
-        assertThat(quizItems.get(0).get("chartPath"),is("/chart.jpg"));
     }
 }
