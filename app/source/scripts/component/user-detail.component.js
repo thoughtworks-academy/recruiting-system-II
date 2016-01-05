25b01cbb1f822bf5a203338a495e7291af683257
wengjiaojiao <419909058@qq.com>
2016-01-05 13:53:11 +0800

    JS:add front stage validate about school,name and major

--- app/source/scripts/component/user-detail.component.js
+++ app/source/scripts/component/user-detail.component.js
@@ -4,6 +4,15 @@ var Input = require('react-bootstrap/lib/Input');
 var UserDetailActions = require('../actions/user-detail-actions');
 var UserDetailStore = require('../store/user-detail-store');
 var Reflux = require('reflux');
+var validate = require("validate.js");
+var constraint = require('../../../mixin/user-detail-constraint');
+
+function getError(validateInfo, field) {
+  if (validateInfo && validateInfo[field] && validateInfo[field].length > 0) {
+    return validateInfo[field][0];
+  }
+  return ""
+}
 
 var UserDetail = React.createClass({
   mixins: [Reflux.connect(UserDetailStore)],
@@ -16,7 +25,10 @@ var UserDetail = React.createClass({
       email: '',
       gender: '',
       major: '',
-      degree: ''
+      degree: '',
+      schoolError: '',
+      nameError: '',
+      majorError:''
     }
   },
 
@@ -60,6 +72,22 @@ var UserDetail = React.createClass({
     this.setState({degree: newState});
   },
 
+  validate: function (event) {
+    var target = event.target;
+    var value = target.value;
+    var name = target.name;
+    var valObj = {};
+
+    valObj[name] = value;
+
+    var result = validate(valObj, constraint);
+    var error = getError(result, name);
+    var stateObj = {};
+
+    stateObj[name + 'Error'] = error;
+    this.setState(stateObj);
+  },
+
   update: function (evt) {
     evt.preventDefault();
 
@@ -73,6 +101,9 @@ var UserDetail = React.createClass({
       degree: this.state.degree
     };
 
+    if(this.state.schoolError !== '' || this.state.nameError !== '' || this.state.majorError !== '') {
+      return ;
+    }
     UserDetailActions.updateUserDetail(userData);
   },
 
@@ -84,16 +115,18 @@ var UserDetail = React.createClass({
             <label htmlFor="inputSchool" className="col-sm-4 col-md-4 control-label">学校</label>
             <div className="col-sm-4 col-md-4">
               <input type="text" className="form-control" id="inputSchool" placeholder="学校"
-                     onChange={this.schoolChange} name="school" value={this.state.school}/>
+                     onChange={this.schoolChange} name="school" value={this.state.school} onBlur={this.validate}/>
             </div>
+            <div className={"error" + (this.state.schoolError === '' ? ' hide' : '')}>{this.state.schoolError}</div>
           </div>
 
           <div className="form-group">
             <label htmlFor="inputName" className="col-sm-4 col-md-4 control-label">姓名</label>
             <div className="col-sm-4 col-md-4">
               <input type="text" className="form-control" id="inputName" placeholder="姓名"
-                     onChange={this.nameChange} name="name" value={this.state.name}/>
+                     onChange={this.nameChange} name="name" value={this.state.name} onBlur={this.validate}/>
             </div>
+            <div className={"error" + (this.state.nameError === '' ? ' hide' : '')}>{this.state.nameError}</div>
           </div>
 
           <div className="form-group">
@@ -128,15 +161,17 @@ var UserDetail = React.createClass({
             <label htmlFor="inputMajor" className="col-sm-4 col-md-4 control-label">专业</label>
             <div className="col-sm-4 col-md-4">
               <input type="text" className="form-control" id="inputMajor" placeholder="专业"
-                     onChange={this.majorChange} name="major" value={this.state.major}/>
+                     onChange={this.majorChange} name="major" value={this.state.major} onBlur={this.validate}/>
             </div>
-            <div></div>
+            <div className={"error" + (this.state.majorError === '' ? ' hide' : '')}>{this.state.majorError}</div>
+
           </div>
 
           <div className="form-group">
             <label htmlFor="inputDegree" className="col-sm-4 col-md-4 control-label">学历学位</label>
             <div className="col-sm-4 col-md-4 degree">
               <Input type="select" placeholder="学历学位" name="degree" value={this.state.degree} onChange={this.degreeChange}>
+                <option value="专科">专科及以下</option>
                 <option value="本科">本科</option>
                 <option value="硕士">硕士</option>
                 <option value="博士">博士</option>
