4263a20725ab6b25ec06f790ac72757ae97c9b25
wengjiaojiao <419909058@qq.com>
2016-01-05 12:08:46 +0800

    JS:add update function and get the user data

--- app/source/scripts/component/user-detail.component.js
+++ app/source/scripts/component/user-detail.component.js
@@ -24,6 +24,58 @@ var UserDetail = React.createClass({
     UserDetailActions.loadUserDetail();
   },
 
+  schoolChange: function (evt) {
+    var newState = evt.target.value;
+
+    this.setState({school: newState});
+  },
+
+  nameChange: function (evt) {
+    var newState = evt.target.value;
+
+    this.setState({name: newState});
+  },
+
+  genderChange: function (evt) {
+    var newState = '';
+    var choose = evt.target.name;
+
+    if(choose === '男') {
+      newState = '男';
+    }else {
+      newState = '女';
+    }
+    this.setState({gender: newState});
+  },
+
+  majorChange: function (evt) {
+    var newState = evt.target.value;
+
+    this.setState({major: newState});
+  },
+
+  degreeChange: function (evt) {
+    var newState = evt.target.value;
+
+    this.setState({degree: newState});
+  },
+
+  update: function (evt) {
+    evt.preventDefault();
+
+    var userData = {
+      school: this.state.school,
+      name: this.state.name,
+      mobilePhone: this.state.mobilePhone,
+      email: this.state.email,
+      gender: this.state.gender,
+      major: this.state.major,
+      degree: this.state.degree
+    };
+
+    UserDetailActions.updateUserDetail(userData);
+  },
+
   render: function () {
 
     return (
@@ -32,7 +84,7 @@ var UserDetail = React.createClass({
             <label htmlFor="inputSchool" className="col-sm-4 col-md-4 control-label">学校</label>
             <div className="col-sm-4 col-md-4">
               <input type="text" className="form-control" id="inputSchool" placeholder="学校"
-                     onChange={this.handleChange} ref="school" value={this.state.school}/>
+                     onChange={this.schoolChange} name="school" value={this.state.school}/>
             </div>
           </div>
 
@@ -40,7 +92,7 @@ var UserDetail = React.createClass({
             <label htmlFor="inputName" className="col-sm-4 col-md-4 control-label">姓名</label>
             <div className="col-sm-4 col-md-4">
               <input type="text" className="form-control" id="inputName" placeholder="姓名"
-                     onChange={this.handleChange} ref="name" value={this.state.name}/>
+                     onChange={this.nameChange} name="name" value={this.state.name}/>
             </div>
           </div>
 
@@ -64,10 +116,10 @@ var UserDetail = React.createClass({
             <label htmlFor="inputGender" className="col-sm-4 col-md-4 control-label">性别</label>
             <div className="col-sm-4 col-md-4">
 
-              <input type="radio" name="options" className="gender"
+              <input type="radio" name="男" className="gender" onChange={this.genderChange}
                      checked={this.state.gender === "男" ? "checked" : ""}/>男
 
-              <input type="radio" name="options" className="gender"
+              <input type="radio" name="女" className="gender" onChange={this.genderChange}
                      checked={this.state.gender === "女" ? "checked" : ""}/>女
             </div>
           </div>
@@ -76,7 +128,7 @@ var UserDetail = React.createClass({
             <label htmlFor="inputMajor" className="col-sm-4 col-md-4 control-label">专业</label>
             <div className="col-sm-4 col-md-4">
               <input type="text" className="form-control" id="inputMajor" placeholder="专业"
-                     onChange={this.handleChange} ref="major" value={this.state.major}/>
+                     onChange={this.majorChange} name="major" value={this.state.major}/>
             </div>
             <div></div>
           </div>
@@ -84,7 +136,7 @@ var UserDetail = React.createClass({
           <div className="form-group">
             <label htmlFor="inputDegree" className="col-sm-4 col-md-4 control-label">学历学位</label>
             <div className="col-sm-4 col-md-4 degree">
-              <Input type="select" placeholder="学历学位" ref="degree" value={this.state.degree}>
+              <Input type="select" placeholder="学历学位" name="degree" value={this.state.degree} onChange={this.degreeChange}>
                 <option value="本科">本科</option>
                 <option value="硕士">硕士</option>
                 <option value="博士">博士</option>
