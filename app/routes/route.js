09f1afe6786dc77bf1a26df4a0ed0adc6757d134
sialvsic <sialvsic@outlook.com>
2016-01-06 23:35:48 +0800

    App:  add dashboard router

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,9 +1,9 @@
-exports.setRoutes = function(app) {
-  app.use('/', require('./routers/index'));
+exports.setRoutes = function (app) {
   app.use('/register', require('./routers/register'));
   app.use('/logic-puzzle', require('./routers/logic-puzzle'));
   app.use('/login', require('./routers/login'));
-  app.use('/user-puzzle',require('./routers/user-puzzle'));
+  app.use('/user-puzzle', require('./routers/user-puzzle'));
   app.use('/start', require('./routers/start'));
   app.use('/user-detail', require('./routers/user-detail'));
+  app.use('/dashboard', require('./routers/dashboard'));
 };
