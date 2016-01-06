83965c665bd747db51bb063d5b613ee76af49e66
purpletianjing <purpletianjing@163.com>
2016-01-06 14:39:02 +0800

    Api: add path'/blankQuizzes/{blankQuizId}/items'

--- api/src/main/java/com/thoughtworks/twars/resource/BlankQuizResource.java
+++ api/src/main/java/com/thoughtworks/twars/resource/BlankQuizResource.java
@@ -12,17 +12,18 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Path("/blankQuizzes")
 public class BlankQuizResource {
     @Inject
     private BlankQuizMapper blankQuizMapper;
+
+    @Inject
     private QuizItemMapper quizItemMapper;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllBlankQuizzes() {
@@ -64,6 +65,61 @@ public class BlankQuizResource {
         }
 
         return Response.status(200).entity(result).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{param}/items")
+    public Response getItems(@PathParam("param") int blankQuizId) {
+
+
+        BlankQuiz blankQuiz = blankQuizMapper.findById(blankQuizId);
+
+        List<QuizItem> easyItems = quizItemMapper.getEasyItems(blankQuiz.getEasyCount());
+        List<QuizItem> normalItems = quizItemMapper.getNormalItems(blankQuiz.getNormalCount());
+        List<QuizItem> hardItems = quizItemMapper.getHardItems(blankQuiz.getHardCount());
+        List<QuizItem> exampleItems = quizItemMapper.getExampleItems();
+
+        List<QuizItem> quizItems = new ArrayList<>();
+
+        quizItems.addAll(easyItems);
+        quizItems.addAll(normalItems);
+        quizItems.addAll(hardItems);
+
+        List quizList = quizItems
+                .stream()
+                .map(BlankQuizResource::buildItemMap)
+                .collect(Collectors.toList());
+        List exampleList = exampleItems
+                .stream()
+                .map(BlankQuizResource::buildExampleMap)
+                .collect(Collectors.toList());
+
+        Map<String, List> result = new HashMap<>();
+        result.put("quizItems", quizList);
+        result.put("exampleItems",exampleList);
+
+        return Response.status(200).entity(result).build();
+    }
+
+    private static Map buildItemMap(QuizItem quizItem) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", quizItem.getId());
+        result.put("initializedBox", quizItem.getInitializedBox());
+        result.put("question", quizItem.getQuestion());
+        result.put("chartPath", quizItem.getChartPath());
+        result.put("description", quizItem.getDescription());
+        return result;
+    }
+    private static Map buildExampleMap(QuizItem quizItem) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", quizItem.getId());
+        result.put("initializedBox", quizItem.getInitializedBox());
+        result.put("question", quizItem.getQuestion());
+        result.put("chartPath", quizItem.getChartPath());
+        result.put("description", quizItem.getDescription());
+        result.put("answer", quizItem.getAnswer());
+        return result;
     }
 
 }
