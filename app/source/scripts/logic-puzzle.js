a0afa61c395462fb4469d10f04a297d042fdee6d
XTWang <meeting_love_lengyi@hotmail.com>
2016-01-11 16:29:07 +0800

    App: add use strict

--- app/source/scripts/logic-puzzle.js
+++ app/source/scripts/logic-puzzle.js
@@ -1,6 +1,10 @@
+/* jshint browser: true */
+
+'use strict';
+
 var $ = global.jQuery = require('jquery');
 var ReactDOM = require('react-dom');
-var React = require('react');
+
 require('../less/logic-puzzle.less');
 require("bootstrap");
 
