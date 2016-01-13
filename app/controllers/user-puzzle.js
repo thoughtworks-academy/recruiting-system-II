6e915a671c0755f4b1508bd0dab02466c4b9d032
Wenjun LIN <linwenjun@163.com>
2016-01-13 10:07:26 +0800

    CheckStyle: Fix checkstyle issue

--- app/controllers/user-puzzle.js
+++ app/controllers/user-puzzle.js
@@ -1,19 +1,14 @@
 'use strict';
 
-//var superAgent = require('superagent');
-//var agent = require('superagent-promise')(superAgent, Promise);
-//var apiServer = require('../configuration').apiServer;
 var userPuzzle = require('../models/user-puzzle');
 
-function UserPuzzleController() {
-
-}
+function UserPuzzleController() {}
 
 UserPuzzleController.prototype.getUserPuzzle = function(req,res){
   var orderId = req.query.orderId;
   var userId = req.session.user.id;
 
-  userPuzzle.getUserPuzzle(orderId,userId)
+  userPuzzle.getUserPuzzle(orderId, userId)
       .then((data) => {
         res.send(data);
       });
