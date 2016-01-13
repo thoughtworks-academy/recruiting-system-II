806b042d9180b41da1c99816aecdd76193b0332c
ming <zsy498716654@live.com>
2016-01-13 17:21:39 +0800

    App: controller Refactory

--- app/controllers/user-puzzle.js
+++ app/controllers/user-puzzle.js
@@ -1,10 +1,15 @@
 'use strict';
 
+var apiRequest = require('../services/api-request');
+var superAgent = require('superagent');
+var apiServer = require('../configuration').apiServer;
 var userPuzzle = require('../models/user-puzzle');
+var async = require('async');
 
-function UserPuzzleController() {}
+function UserPuzzleController() {
+}
 
-UserPuzzleController.prototype.getUserPuzzle = function(req,res){
+UserPuzzleController.prototype.getUserPuzzle = function (req, res) {
   var orderId = req.query.orderId;
   var userId = req.session.user.id;
 
@@ -14,16 +19,73 @@ UserPuzzleController.prototype.getUserPuzzle = function(req,res){
       });
 };
 
-UserPuzzleController.prototype.saveAnswer = function(req,res){
+UserPuzzleController.prototype.saveAnswer = function (req, res) {
   var orderId = req.body.orderId;
   var userAnswer = req.body.userAnswer;
   var userId = req.session.user.id;
-  userPuzzle.saveAnswer(orderId,userAnswer,userId)
+  userPuzzle.saveAnswer(orderId, userAnswer, userId)
       .then((isSucceed) => {
-        if(isSucceed === true){
+        if (isSucceed === true) {
           res.sendStatus(200);
         }
       });
+};
+
+UserPuzzleController.prototype.createUser = function (req, res) {
+  var userId = 10;
+  var quizItems, quizExamples, blankQuizId, paperId;
+  var userPuzzleUrl = 'papers/enrollment';
+
+  async.waterfall([
+
+    function (done) {
+
+      userPuzzle.findOne({userId: userId}, (err, data) => {
+        if (err) {
+          done(err, data);
+        } else {
+          done(!!data, data);
+        }
+      });
+    }, function (data, done) {
+      apiRequest.get(userPuzzleUrl, done);
+    }, function (responds, done) {
+
+      var quizzes = responds.body.sections[0].quizzes[0];
+      blankQuizId = quizzes.id;
+      paperId = responds.body.id;
+      var itemsUri = quizzes.items.uri;
+
+      done(null, itemsUri);
+    }, function (itemsUri, done) {
+      apiRequest.get(itemsUri, done);
+    }, function (responds, done) {
+
+      quizItems = responds.body.quizItems;
+      quizExamples = responds.body.exampleItems;
+
+      var isNotExist = true;
+
+      done(null, isNotExist);
+    }, function (isNotExist, done) {
+
+      userPuzzle.create({
+        userId: userId,
+        quizItems: quizItems,
+        quizExamples: quizExamples,
+        blankQuizId: blankQuizId,
+        paperId: paperId
+      },done);
+
+    }
+  ], function (err) {
+    if (true !== err && err) {
+      res.statusCode(500);
+      res.send({status: 500, message: '服务器错误'});
+    } else {
+      res.send({status: 200, message: '初始化成功!'});
+    }
+  });
 };
 
 module.exports = UserPuzzleController;
--- app/routes/routers/user-puzzle.js
+++ app/routes/routers/user-puzzle.js
@@ -2,17 +2,12 @@
 
 var express = require('express');
 var router = express.Router();
-var superAgent = require('superagent');
-var agent = require('superagent-promise')(superAgent, Promise);
-var userPuzzle = require('../../models/user-puzzle');
 var constant = require('../../mixin/back-constant.json');
 var UserPuzzleController = require('../../controllers/user-puzzle');
 var userPuzzleController = new UserPuzzleController();
 
 router.post('/save', userPuzzleController.saveAnswer);
 
-router.get('/createUser', function (req, res) {
-  userPuzzle.initialDB(req, res);
-});
+router.get('/createUser', userPuzzleController.createUser);
 
 module.exports = router;
