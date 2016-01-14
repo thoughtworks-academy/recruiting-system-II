0019eb21ed6af4f92bebb8d90959378b11afa269
wengjiaojiao <419909058@qq.com>
2016-01-14 22:20:43 +0800

    APP:fix the bug about clicking the icon and it wasn't change the state when it's disabled

--- app/source/scripts/component/programme-sidebar.component.js
+++ app/source/scripts/component/programme-sidebar.component.js
@@ -6,14 +6,14 @@ var React = require('react');
 
 var ProgrammeSidebar = React.createClass({
 
-  changeIcon:function(state) {
+  changeIcon: function (state) {
     var icon = 'h4 fa fa-';
     var iconList = ['lock', '', 'clock-o', 'check-circle', 'times-circle'];
-    var stateCode = [0,1,2,3,4];
+    var stateCode = [0, 1, 2, 3, 4];
 
     stateCode.forEach((item, index) => {
-      if(state === item) {
-        icon =  icon + iconList[index];
+      if (state === item) {
+        icon = icon + iconList[index];
       }
     });
     return icon;
@@ -22,7 +22,7 @@ var ProgrammeSidebar = React.createClass({
   handleClick: function (mark, evt) {
     var name = evt.target.offsetParent.className;
 
-    if(name.includes('disabled') === false) {
+    if (name.includes('disabled') === false) {
       this.props.onChangeState(mark);
     }
   },
@@ -30,21 +30,21 @@ var ProgrammeSidebar = React.createClass({
   render() {
     var tags = [];
 
-    for(var i = 0; i < 10; i ++ ) {
+    for (var i = 0; i < 10; i++) {
       var index = i + 1;
 
-      tags.push({mark:index, value: '第' + index + '题', state: 2});
+      tags.push({mark: index, value: '第' + index + '题', state: 0});
     }
     var itemHtml = tags.map((item, index) => {
       var classStr = 'list-group-item ' + (item.mark === this.props.currentTopicNumber ? 'selected' : '')
-                                        + (item.state === 0 ? ' disabled' : '');
-
+          + (item.state === 0 ? ' disabled' : '');
       return (
           <a className={classStr} href="javascript:void(0)" key={index}
              onClick={this.handleClick.bind(null, item.mark)}>
             <div className="row">
               <div className="col-xs-9 h5 text-center">{item.value}</div>
-              <div className="col-xs-3"><i className={this.changeIcon(item.state)}/></div>
+              <div className={'col-xs-3' + (item.state === 0 ? ' disabled' : '')}>
+                <i className={this.changeIcon(item.state)}/></div>
             </div>
           </a>
       );
