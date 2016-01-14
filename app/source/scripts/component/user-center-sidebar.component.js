269723f181e28ac2f9ff81611f7bde7c4af02d78
wengjiaojiao <419909058@qq.com>
2016-01-14 13:32:52 +0800

    APP:adjust the color about user center sidebar

--- app/source/scripts/component/user-center-sidebar.component.js
+++ app/source/scripts/component/user-center-sidebar.component.js
@@ -12,26 +12,34 @@ var UserCenterSide = React.createClass({
 
   render() {
     var tags = [
-      {mark: 'userDetail', value: '个人中心', icon: 'fa-user-plus'},
-      {mark: 'password', value: '修改密码', icon: 'fa-user-secret'}
+      {mark: 'userDetail', value: '个人信息'},
+      {mark: 'password', value: '修改密码'}
     ];
 
     var itemHtml = tags.map((item, index) => {
-      var classStr = 'list-group-item ' + (item.mark === this.props.currentState ? 'active' : '');
+      var classStr = 'list-group-item ' + (item.mark === this.props.currentState ? 'selected' : '');
 
       return (
-          <a className={classStr} key={index} onClick={this.handleClick.bind(null, item.mark)}>
+          <a className={classStr} href="javascript:void(0)" key={index} onClick={this.handleClick.bind(null, item.mark)}>
             <div className="row">
               <div className="col-xs-9 h4 text-center">{item.value}</div>
-              <div className="col-xs-3"><i className={'user-center-nav-icon h4 fa-lg fa ' + item.icon}></i></div>
+              <div className="col-xs-3"></div>
             </div>
           </a>
       );
     });
 
     return (
+
         <div className="col-md-3 col-sm-3 col-xs-12">
+
           <div className="list-group">
+            <div className="list-group-item active">
+              <div className="row">
+                <div className="col-xs-9 h4 text-center">个人中心</div>
+                <div className="col-xs-3"><i className={'user-center-nav-icon h4 fa-lg fa fa-user-plus'}></i></div>
+              </div>
+            </div>
             {itemHtml}
           </div>
         </div>
