2d4afd5eea8213fb8ccc57dcb629e5ca69429b70
wengjiaojiao <419909058@qq.com>
2016-01-14 11:24:05 +0800

    APP:use bootstrap to replace the react-bootstrap and write the user-center-sidebar

--- app/source/scripts/component/user-center-sidebar.component.js
+++ app/source/scripts/component/user-center-sidebar.component.js
@@ -5,31 +5,34 @@ var Nav = require('react-bootstrap/lib/Nav');
 var NavItem = require('react-bootstrap/lib/NavItem');
 
 var UserCenterSide = React.createClass({
-  changeState: function (evt) {
-    var name = evt.target.innerText === '个人中心' ? 'userDetail' : 'password';
 
-    this.props.onChangeState(name);
+  handleClick: function (mark) {
+    this.props.onChangeState(mark);
   },
 
   render() {
     var tags = [
-      {mark: 'userDetail', value: '个人中心', icon: 'fa fa-user-secret'},
-      {mark: 'password', value: '修改密码', icon: 'fa fa-user-plus'}
+      {mark: 'userDetail', value: '个人中心', icon: 'fa-user-plus'},
+      {mark: 'password', value: '修改密码', icon: 'fa-user-secret'}
     ];
+
+    var itemHtml = tags.map((item, index) => {
+      var classStr = 'list-group-item ' + (item.mark === this.props.currentState ? 'active' : '');
+
+      return (
+          <a className={classStr} key={index} onClick={this.handleClick.bind(null, item.mark)}>
+            <div className="row">
+              <div className="col-xs-9 h4 text-center">{item.value}</div>
+              <div className="col-xs-3"><i className={'user-center-nav-icon h4 fa-lg fa ' + item.icon}></i></div>
+            </div>
+          </a>
+      );
+    });
 
     return (
         <div className="col-md-3 col-sm-3 col-xs-12">
-          <div className="sidebar">
-            <ul>
-              <Nav bsStyle="pills" stacked activeKey={this.props.currentState}>
-                {tags.map((item, index) => {
-                  return (
-                      <NavItem eventKey={item.mark} href="javascript:void(0)" onClick={this.changeState} key={index}>{item.value}
-                        <i className={item.icon}></i></NavItem>
-                  );
-                })}
-              </Nav>
-            </ul>
+          <div className="list-group">
+            {itemHtml}
           </div>
         </div>
     );
