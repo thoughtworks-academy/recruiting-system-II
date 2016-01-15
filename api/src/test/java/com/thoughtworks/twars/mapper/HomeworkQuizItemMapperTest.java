a8d20d7cf1ee50191d5d860310ddea2442069078
purpletianjing <purpletianjing@163.com>
2016-01-15 17:03:37 +0800

    Api: test homeworkQuizItemMapper

---
+++ api/src/test/java/com/thoughtworks/twars/mapper/HomeworkQuizItemMapperTest.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.twars.mapper;
+
+import com.thoughtworks.twars.bean.HomeworkQuizItem;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class HomeworkQuizItemMapperTest extends TestBase {
+    private HomeworkQuizItemMapper homeworkQuizItemMapper;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        homeworkQuizItemMapper = session.getMapper(HomeworkQuizItemMapper.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+    @Test
+    public void should_return_all_homework_quiz_items() throws Exception {
+        List<HomeworkQuizItem> homeworkQuizItems = homeworkQuizItemMapper.findByHomeworkQuizId(1);
+        assertThat(homeworkQuizItems.size(), is(0));
+    }
+
+}
