80c1c6241e4fcd7e464d04ce7da7862e607b171f
wengjiaojiao <419909058@qq.com>
2016-01-15 09:30:52 +0800

    APP:remove the jquery demo

--- app/source/scripts/programme.js
+++ app/source/scripts/programme.js
@@ -1,19 +1,9 @@
 'use strict';
 
-var $ = global.jQuery = require('jquery');
-require('bootstrap');
-
 require('../less/programme.less');
 var ReactDom = require('react-dom');
 var Navigation = require('./component/navigation.component');
 var ProgrammeApp = require('./component/programme-app.component');
-
-$(function () {
-  $('.nav a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-  });
-});
 
 ReactDom.render(
     <Navigation />,
