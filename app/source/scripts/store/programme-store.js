130ba554395d6f3f521140a53b4f07e458cc927e
wengjiaojiao <419909058@qq.com>
2016-01-15 11:13:16 +0800

    APP:create the store about programme

--- app/source/scripts/store/programme-store.js
+++ app/source/scripts/store/programme-store.js
@@ -1,3 +1,16 @@
-/**
- * Created by Jessica on 1/15/16.
- */
+'use strict';
+
+var Reflux = require('reflux');
+var ProgrammeActions = require('../actions/programme-actions');
+var request = require('superagent');
+
+var ProgrammeStore = Reflux.createStore({
+  listenables: [ProgrammeActions],
+
+  onLoadTopicState: function () {
+
+
+  }
+});
+
+module.exports = ProgrammeStore;
