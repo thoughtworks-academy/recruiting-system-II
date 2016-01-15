09ff3d35351bda5bfeaf57ece07768df04359967
wengjiaojiao <419909058@qq.com>
2016-01-15 15:54:56 +0800

    APP:add if to judge the httpcode

--- app/source/scripts/store/programme-store.js
+++ app/source/scripts/store/programme-store.js
@@ -3,13 +3,21 @@
 var Reflux = require('reflux');
 var ProgrammeActions = require('../actions/programme-actions');
 var request = require('superagent');
+var constant = require('../../../mixin/constant');
+
 
 var ProgrammeStore = Reflux.createStore({
   listenables: [ProgrammeActions],
 
   onLoadTopicState: function () {
-
-
+    request.get('/programme/get-list')
+        .set('Content-Type', 'application/json')
+        .end((err, res) => {
+          if (err || res.status !== constant.httpCode.OK) {
+            return;
+          }
+          this.trigger(res.body);
+        });
   }
 });
 
