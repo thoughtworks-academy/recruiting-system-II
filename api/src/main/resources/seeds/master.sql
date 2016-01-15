ae1dd9b42d7759adb1c2cd294b2c82edca8b8482
purpletianjing <purpletianjing@163.com>
2016-01-15 10:44:15 +0800

    Api: add data for homeworkQuiz

--- api/src/main/resources/seeds/master.sql
+++ api/src/main/resources/seeds/master.sql
@@ -87,3 +87,8 @@ INSERT INTO blankQuizScoreSheet VALUES (NULL ,1, 1, 2, "23",1);
 INSERT INTO blankQuizScoreSheet VALUES (NULL ,1, 1, 3, "34",1);
 INSERT INTO blankQuizScoreSheet VALUES (NULL ,1, 1, 4, "45",1);
 
+TRUNCATE homeworkQuiz;
+INSERT INTO homeworkQuiz VALUES (NULL ,3);
+INSERT INTO homeworkQuiz VALUES (NULL ,3);
+INSERT INTO homeworkQuiz VALUES (NULL ,3);
+
