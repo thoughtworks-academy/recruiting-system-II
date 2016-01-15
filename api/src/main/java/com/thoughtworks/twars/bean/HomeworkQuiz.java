bbc3243aa48f134773fa77cf7952c8d768f537fd
purpletianjing <purpletianjing@163.com>
2016-01-15 09:22:34 +0800

    Api: move to bean

---
+++ api/src/main/java/com/thoughtworks/twars/bean/HomeworkQuiz.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.twars.bean;
+
+public class HomeworkQuiz {
+    private int id;
+    private int sectionId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSectionId() {
+        return sectionId;
+    }
+
+    public void setSectionId(int sectionId) {
+        this.sectionId = sectionId;
+    }
+}
