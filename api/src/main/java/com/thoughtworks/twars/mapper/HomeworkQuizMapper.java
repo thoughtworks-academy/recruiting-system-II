62e3bebf9ad9b795034488d01d2475e808e44649
Danping YAO <anlihuer@outlook.com>
2016-01-20 22:06:17 +0800

    Api: Change the homeworkQuiz

---
+++ api/src/main/java/com/thoughtworks/twars/mapper/HomeworkQuizMapper.java
@@ -0,0 +1,5 @@
+package com.thoughtworks.twars.mapper;
+
+public interface HomeworkQuizMapper {
+
+}
--- api/src/test/java/com/thoughtworks/twars/mapper/HomeWorkQuizMapperTest.java
+++ api/src/test/java/com/thoughtworks/twars/mapper/HomeWorkQuizMapperTest.java
@@ -1,32 +1,15 @@
 package com.thoughtworks.twars.mapper;
 
-import com.thoughtworks.twars.bean.HomeworkQuiz;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 
 public class HomeWorkQuizMapperTest extends TestBase {
-    private HomeWorkQuizMapper homeWorkQuizMapper;
+    private HomeworkQuizMapper homeworkQuizMapper;
 
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        homeWorkQuizMapper = session.getMapper(HomeWorkQuizMapper.class);
+        homeworkQuizMapper = session.getMapper(HomeworkQuizMapper.class);
     }
 
-    @After
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-//    @Test
-//    public void should_return_some_home_work_quizzes_by_given_section_id() throws Exception {
-//        List<HomeworkQuiz> homeworkQuizzes = homeWorkQuizMapper.findBySectionId(1);
-//        assertThat(homeworkQuizzes.size(), is(0));
-//    }
 }
