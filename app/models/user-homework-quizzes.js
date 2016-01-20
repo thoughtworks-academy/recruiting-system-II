899a29cb1f9b013594fd06f4577795383c19b2f9
XTWang <meeting_love_lengyi@hotmail.com>
2016-01-20 12:29:12 +0800

    App: Add unlockNext method

--- app/models/user-homework-quizzes.js
+++ app/models/user-homework-quizzes.js
@@ -2,6 +2,7 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var constant = require('../mixin/constant');
 
 var userHomeworkQuizzesSchema = new Schema({
   userId: Number,
@@ -36,6 +37,34 @@ userHomeworkQuizzesSchema.statics.initUserHomeworkQuizzes = function (userId, id
         userId: userId,
         quizzes: quizzes
       }, callback);
+    }
+  });
+};
+
+userHomeworkQuizzesSchema.statics.unlockNext = function (userId, callback) {
+  this.findOne({userId: userId}, function (err, data) {
+    if (err || !data) {
+      callback(err || new Error('user is not allowed'));
+    } else {
+      var locked = 0;
+      var success = 0;
+
+      data.quizzes.forEach(function (quiz) {
+        if (quiz.locked) {
+          locked++;
+        } else if (quiz.status === constant.homeworkQuizzesStatus.SUCCESS) {
+          success++;
+        }
+      });
+
+      if (data.quizzes.length === (locked + success)) {
+        data.quizzes[success].status = 1;
+        data.quizzes[success].locked = false;
+
+        data.save(callback);
+      }else {
+        callback(null, null);
+      }
     }
   });
 };
