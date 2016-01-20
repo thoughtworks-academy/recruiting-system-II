4fd819154a9c653fbe156c97ec0d610380d96bd1
Danping YAO <anlihuer@outlook.com>
2016-01-20 22:04:37 +0800

    Api: Create the homeworkSubmit insert function and test

---
+++ api/src/main/java/com/thoughtworks/twars/mapper/HomeworkSubmitMapper.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.twars.mapper;
+
+import com.thoughtworks.twars.bean.HomeworkSubmit;
+
+public interface HomeworkSubmitMapper {
+    int insertHomeworkSubmit(HomeworkSubmit homeworkSubmit);
+}
