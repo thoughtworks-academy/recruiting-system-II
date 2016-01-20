740c44f8ac95c0f3ba02c2968e9563c62280fd99
purpletianjing <purpletianjing@163.com>
2016-01-20 11:17:48 +0800

    Api: modify find to findByScoreSheetId

--- api/src/main/java/com/thoughtworks/twars/mapper/BlankQuizSubmitMapper.java
+++ api/src/main/java/com/thoughtworks/twars/mapper/BlankQuizSubmitMapper.java
@@ -2,7 +2,9 @@ package com.thoughtworks.twars.mapper;
 
 import com.thoughtworks.twars.bean.BlankQuizSubmit;
 
+import java.util.List;
+
 public interface BlankQuizSubmitMapper {
     int insertBlankQuizSubmit(BlankQuizSubmit blankQuizSubmit);
-    BlankQuizSubmit findOne(int scoreSheetId);
+    List<BlankQuizSubmit> findByScoreSheetId(int scoreSheetId);
 }
