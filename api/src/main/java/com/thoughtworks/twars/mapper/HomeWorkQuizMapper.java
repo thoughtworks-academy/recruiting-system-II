d8160f56ebbb5d40368048f271328eefa87cb97d
Danping YAO <anlihuer@outlook.com>
2016-01-20 17:25:23 +0800

    RAML: Add field for the request scoresheet

--- api/src/main/java/com/thoughtworks/twars/mapper/HomeWorkQuizMapper.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.twars.mapper;
-
-import com.thoughtworks.twars.bean.HomeworkQuiz;
-
-import java.util.List;
-
-public interface HomeWorkQuizMapper {
-    List<HomeworkQuiz> findBySectionId(int sectionId);
-}
--- api/src/main/java/com/thoughtworks/twars/mapper/HomeworkQuizScoreSheetMapper.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.twars.mapper;
-
-import com.thoughtworks.twars.bean.HomeworkQuizScoreSheet;
-
-public interface HomeworkQuizScoreSheetMapper {
-
-    int insertHomeworkQuizScoreSheet(
-            HomeworkQuizScoreSheet homeworkQuizScoreSheet);
-}
