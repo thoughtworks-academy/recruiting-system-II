7d2bae3a81f34759001944cb317f64048871d1c2
purpletianjing <purpletianjing@163.com>
2016-01-20 11:50:00 +0800

    Api: add findByBlankQuizSubmitId

--- api/src/main/java/com/thoughtworks/twars/mapper/ItemPostMapper.java
+++ api/src/main/java/com/thoughtworks/twars/mapper/ItemPostMapper.java
@@ -2,6 +2,9 @@ package com.thoughtworks.twars.mapper;
 
 import com.thoughtworks.twars.bean.ItemPost;
 
+import java.util.List;
+
 public interface ItemPostMapper {
     int insertItemPost(ItemPost itemPost);
+    List<ItemPost> findByBlankQuizSubmit(int blankQuizSubmitId);
 }
