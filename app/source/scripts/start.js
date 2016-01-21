d7e48ae8d8dd4b8792f8d301a4eb944450869092
wengjiaojiao <419909058@qq.com>
2016-01-21 13:21:30 +0800

    APP:add jquery import and show the model

--- app/source/scripts/start.js
+++ app/source/scripts/start.js
@@ -3,6 +3,8 @@
 var ReactDom = require('react-dom');
 var StartInfo = require('./component/start-info.component');
 var Navigation = require('./component/navigation.component');
+var $ = global.jQuery = require('jquery');
+require('bootstrap');
 require('../less/start.less');
 
 ReactDom.render(
