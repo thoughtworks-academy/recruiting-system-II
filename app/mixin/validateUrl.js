a16857906d627441e640cbb7f8f27438eed8f96e
ming <zsy498716654@live.com>
2016-01-21 17:53:59 +0800

    APP: add validateUrl method in mixin

---
+++ app/mixin/validateUrl.js
@@ -0,0 +1,17 @@
+var VerEx = require('verbal-expressions');
+
+function isGitHubRepoUrl(TestUrl){
+  var rules = VerEx
+      .VerEx()
+      .startOfLine()
+      .maybe('https://')
+      .then('github.com/')
+      .anythingBut(' ')
+      .then('/')
+      .anythingBut(' ')
+      .endOfLine();
+
+  return !!rules.test(TestUrl);
+}
+
+module.exports = isGitHubRepoUrl;
