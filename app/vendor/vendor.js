1e3fe20799c8e905e10758fa03f6852ba711c608
simpletrain <zsy498716654@live.com>
2016-01-28 18:07:55 +0800

    Style: Reformate

--- app/vendor/vendor.js
+++ app/vendor/vendor.js
@@ -8,18 +8,18 @@ function Vendor() {
 
 }
 
-Vendor.prototype.getBranches = function(req, res){
+Vendor.prototype.getBranches = function (req, res) {
   var originUrl = req.query.url;
   var list = originUrl.split('/');
-  var apiUrl = 'https://api.github.com/repos/'+ list[3] +'/' + list[4] +'/branches';
+  var apiUrl = 'https://api.github.com/repos/' + list[3] + '/' + list[4] + '/branches';
   superAgent.get(apiUrl)
       .set('Content-Type', 'application/json')
       .set(githubUser.header, githubUser.value)
       .end((err, result) => {
-        if(result.body.message === 'Not Found'){
+        if (result.body.message === 'Not Found') {
           res.send({message: 'Not Found'});
         } else {
-          res.send({message: 'Succeed',data: result.body});
+          res.send({message: 'Succeed', data: result.body});
         }
       });
 };
