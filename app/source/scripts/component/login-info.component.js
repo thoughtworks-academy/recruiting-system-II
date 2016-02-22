783bf5bd85bd077daa20aa8ec1035a059816d193
Evan-Leee <EvanLee_@outlook.com>
2016-02-22 16:40:43 +0800

    App: delete the third login partner

--- app/source/scripts/component/login-info.component.js
+++ app/source/scripts/component/login-info.component.js
@@ -28,20 +28,6 @@ var LoginInfo = React.createClass({
               {this.state.isLoginState ? '立即注册' : '立即登录'}
             </a>
           </div>
-
-          <div className="third-party">
-            <div className="title">合作方账号登录</div>
-            <ul className="third-party-entry">
-              <li className="qq"><a href="#"></a></li>
-              &nbsp;
-              <li className="weibo"><a href="#"></a></li>
-              &nbsp;
-              <li className="weixin"><a href="#"></a></li>
-              &nbsp;
-              <li className="github"><a href="/login/github"></a></li>
-              &nbsp;
-            </ul>
-          </div>
         </div>
     );
   }
