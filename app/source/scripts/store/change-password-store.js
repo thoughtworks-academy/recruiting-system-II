ad23f2a740dce22d949ea243379909c40b3b8adb
simpletrain <zsy498761554@live.com>
2016-02-22 16:05:03 +0800

    App: Refactor

--- app/source/scripts/store/change-password-store.js
+++ app/source/scripts/store/change-password-store.js
@@ -18,12 +18,18 @@ var ChangePasswordStore = Reflux.createStore({
         })
         .end((err, req) => {
           if (req.body.status === constant.httpCode.OK) {
-            this.trigger({success: true});
-            this.trigger({isRespond: false});
-            this.trigger({oldPassword:'',newPassword: '', confirmPassword: ''});
+            this.trigger({
+              success: true,
+              isRespond: false,
+              oldPassword: '',
+              newPassword: '',
+              confirmPassword: ''
+            });
           } else if (req.body.status === constant.httpCode.BAD_REQUEST) {
-            this.trigger({oldPasswordError: constant.changePassword.ERROR});
-            this.trigger({isRespond: false});
+            this.trigger({
+              isRespond: false,
+              oldPasswordError: constant.changePassword.ERROR
+            });
           } else {
             console.log('error');
           }
