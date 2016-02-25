1e3f72a9e4ac25b6bf159e355d4adfd7143d9be7
wengjiaojiao <419909058@qq.com>
2016-02-25 16:04:09 +0800

    APP:add OnAgreeDeal function in store

--- app/source/scripts/store/start/start-store.js
+++ app/source/scripts/store/start/start-store.js
@@ -1,3 +1,30 @@
-/**
- * Created by Jessica on 2/25/16.
- */
+'use strict';
+
+var Reflux = require('reflux');
+var StartActions = require('../../actions/start/start-actions');
+var superAgent = require('superagent');
+var page = require('page');
+var errorHandler = require('../../../../tools/error-handler');
+var constant = require('../../../../mixin/constant');
+
+var StartStore = Reflux.createStore({
+  listenables: [StartActions],
+
+  onAgreeDeal:function(isAgree) {
+    superAgent.put('/logic-puzzle/dealAgree')
+      .set('Content-type', 'application/json')
+      .send({
+        dealAgree: isAgree
+      })
+      .use(errorHandler)
+      .end((err, req) => {
+        if(req.body.status === constant.httpCode.OK) {
+          page('logic-puzzle.html');
+        }else {
+          console.log('please agree the deal');
+        }
+      });
+  }
+});
+
+module.exports = StartStore;
