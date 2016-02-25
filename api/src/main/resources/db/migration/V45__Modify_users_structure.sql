0c0d71d717b09a8675c2ddf74bdcf5f266279edf
purpletianjing <TianjingSun@outlook.com>
2016-02-25 10:27:17 +0800

    SQL: modify fileds to not null

---
+++ api/src/main/resources/db/migration/V45__Modify_users_structure.sql
@@ -0,0 +1,3 @@
+alter table users modify column email VARCHAR(128) NOT NULL;
+alter table users modify column mobilePhone VARCHAR(64) NOT NULL;
+alter table users modify column password VARCHAR(128) NOT NULL;
