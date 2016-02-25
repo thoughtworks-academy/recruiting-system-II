61fe062657a5790b7885691b1688ab0fe6d2e765
wengjiaojiao <419909058@qq.com>
2016-02-25 16:00:57 +0800

    APP:add agreeDeal action

--- app/source/scripts/actions/start/start-actions.js
+++ app/source/scripts/actions/start/start-actions.js
@@ -1,3 +1,9 @@
-/**
- * Created by Jessica on 2/25/16.
- */
+'use strict';
+
+var Reflux = require('reflux');
+
+var StartActions = Reflux.createActions([
+  'agreeDeal'
+]);
+
+module.exports = StartActions;
