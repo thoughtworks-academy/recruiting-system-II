f1797821ecc6f1893ca6031bfc2ea6d469f8e9dd
Evan-Leee <EvanLee_@outlook.com>
2016-02-26 10:07:42 +0800

    App: move the style of error-handler to less

--- app/tools/error-handler.js
+++ app/tools/error-handler.js
@@ -14,18 +14,13 @@ var errorHandler = function(req) {
 function addErrorMessage() {
   var errorMessageBlock = document.createElement('DIV');
   errorMessageBlock.setAttribute('id','alert');
-  errorMessageBlock.style.marginBottom = 0;
-  errorMessageBlock.style.position = 'absolute';
-  errorMessageBlock.style.top = 0;
-  errorMessageBlock.style.left = 0;
-  errorMessageBlock.style.width = '100%';
-
   document.body.appendChild(errorMessageBlock);
 
   ReactDom.render(
       <div className="alert alert-danger alert-dismissible text-center fade in" id="alert" role="alert">
         <button type="button" className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <strong>哦!糟了!</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;看起来我们的服务器出了一些问题!
+        <span>哦!糟了!</span>
+        <span>看起来我们的服务器出了一些问题!</span>
       </div>,
       document.getElementById('alert')
   );
