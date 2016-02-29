699753dcb8dc546366cfc357fe959975b3406e24
purpletianjing <TianjingSun@outlook.com>
2016-02-29 14:43:23 +0800

    sql: modify mobilePhone of the first test data

--- api/src/test/resources/db/migration/V24_2__Add_test_data_for_user_table.sql
+++ api/src/test/resources/db/migration/V24_2__Add_test_data_for_user_table.sql
@@ -1,6 +1,6 @@
 TRUNCATE users;
-INSERT INTO users values(null, "test@163.com", "12345678901", "550e1bafe077ff0b0b67f4e32f29d751", 1451117203);
-INSERT INTO users values(null, "test2@qq.com", "12345678902", "550e1bafe077ff0b0b67f4e32f29d751", 1451117300);
+INSERT INTO users values(null, "test@163.com", "18798037893", "550e1bafe077ff0b0b67f4e32f29d751", 1451117203);
+INSERT INTO users values(null, "test2@qq.com", "18087839393", "550e1bafe077ff0b0b67f4e32f29d751", 1451117300);
 INSERT INTO users values(null, "wjj@qq.com", "18740404040", "550e1bafe077ff0b0b67f4e32f29d751", 1451368017);
 INSERT INTO users values(null, "ydp@qq.com", "18730304826", "550e1bafe077ff0b0b67f4e32f29d751", 1451370576);
 INSERT INTO users values(null, "lwj@qq.com", "18740362964", "550e1bafe077ff0b0b67f4e32f29d751", 1451370686);
