0aac52516bc8aa0a3a3a84b27a44fa9e74398d5c
purpletianjing <TianjingSun@outlook.com>
2016-02-29 15:19:26 +0800

    Api: format

--- api/src/main/java/com/thoughtworks/twars/resource/LoginResource.java
+++ api/src/main/java/com/thoughtworks/twars/resource/LoginResource.java
@@ -32,7 +32,8 @@ public class LoginResource extends Resource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response createUser(User user) {
 
-        Pattern mobilePhoneMatches = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+        Pattern mobilePhoneMatches = Pattern
+                .compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
         Matcher mobilePhoneMatcher = mobilePhoneMatches.matcher(user.getEmail());
         String emailMatches = "\\p{Alpha}\\w{2,15}[@][a-z0-9]{3,}[.]\\p{Lower}{2,}";
 
@@ -40,8 +41,7 @@ public class LoginResource extends Resource {
 
         if (mobilePhoneMatcher.matches()) {
             resultUser = userMapper.getUserByMobilePhoneAndPassWord(user);
-        }
-        else if (user.getEmail().matches(emailMatches)) {
+        } else if (user.getEmail().matches(emailMatches)) {
 
             resultUser = userMapper.getUserByEmailAndPassWord(user);
         }
@@ -54,23 +54,23 @@ public class LoginResource extends Resource {
                 .getLoginDetailByUserId(resultUser.getId());
 
 
-        if(loginDetails.size() != 0 && loginDetails.get(loginDetails.size() - 1).getFlag() == 1) {
+        if (loginDetails.size() != 0 && loginDetails.get(loginDetails.size() - 1).getFlag() == 1) {
             loginDetailMapper.updateLoginDetail(loginDetails
                     .get(loginDetails.size() - 1).getToken());
         }
 
         loginDetailMapper.insertLoginDetail(resultUser.getId());
 
-        List<LoginDetail> loginDetailList = loginDetailMapper
-                .getLoginDetailByUserId(resultUser.getId());
-
         Map<String, Object> map = new HashMap<>();
         Map<String, String> userInfo = new HashMap<>();
-        String token = loginDetailList.get(loginDetailList.size() - 1).getToken();
 
         map.put("id", resultUser.getId());
         userInfo.put("uri", "users/" + resultUser.getId());
         map.put("userInfo", userInfo);
+        
+        List<LoginDetail> loginDetailList = loginDetailMapper
+                .getLoginDetailByUserId(resultUser.getId());
+        String token = loginDetailList.get(loginDetailList.size() - 1).getToken();
 
         return Response.ok(map).header("token", token).build();
     }
