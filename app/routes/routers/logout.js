910f83637ee537dd2dec0038789829041fab80a1
sialvsic <sialvsic@outlook.com>
2016-03-01 21:08:56 +0800

    App: format and delete one empty line

--- app/routes/routers/logout.js
+++ app/routes/routers/logout.js
@@ -3,10 +3,9 @@
 var express = require('express');
 var router = express.Router();
 
-var LogoutController  = require('../../controllers/logout-controller');
-var logoutController  = new  LogoutController();
+var LogoutController = require('../../controllers/logout-controller');
+var logoutController = new LogoutController();
 
 router.get('/', logoutController.logout);
-
 
 module.exports = router;
