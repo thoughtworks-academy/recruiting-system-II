9e60114f9adbc690a10db36ad1cb0a8af0fb09a1
wengjiaojiao <419909058@qq.com>
2016-03-01 14:19:03 +0800

    APP:control the max data to choose

--- app/source/scripts/component/user-center/user-center-birthday.component.js
+++ app/source/scripts/component/user-center/user-center-birthday.component.js
@@ -7,11 +7,14 @@ var Calendar = require('react-input-calendar');
 var UserCenterBirthday = React.createClass({
 
   render: function () {
+    var data = new Date();
+    var today = (data.getMonth() + 1) + '/' + data.getDate() + '/' + data.getFullYear();
 
     return (
         <div>
           <Calendar
               format="DD/MM/YYYY"
+              maxDate={today}
           />
         </div>
 
