01295e1256b4ad192a477ac45fe881624aa54ce0
wengjiaojiao <419909058@qq.com>
2016-03-02 11:16:14 +0800

    APP:use constant to replace the magic number

--- app/mixin/time-turner.js
+++ app/mixin/time-turner.js
@@ -1,13 +1,14 @@
 'use strict';
+var TIME = require('./constant').time;
 
 function timeTurner() {
 }
 
-timeTurner.timeToStamp = function(time) {
-  return (new Date(time)).getTime()/1000;
+timeTurner.timeToStamp = function (time) {
+  return (new Date(time)).getTime() / TIME.MILLISECOND_PER_SECONDS;
 };
 
 timeTurner.stampToTime = function (stamp) {
-  return new Date(parseInt(stamp) *1000).toLocaleDateString().replace(/:\d{1,2}$/,' ');
+  return new Date(parseInt(stamp) * TIME.MILLISECOND_PER_SECONDS).toLocaleDateString().replace(/:\d{1,2}$/, ' ');
 };
 module.exports = timeTurner;
