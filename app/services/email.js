a1fe05ec79f6acee533bc4ab2561db676458ed69
sialvsic <sialvsic@outlook.com>
2016-03-02 11:07:49 +0800

    App: add superlink in address

--- app/services/email.js
+++ app/services/email.js
@@ -9,8 +9,8 @@ var emailServer = {
 
   sendEmail: function (emailTarget, linkAddress, callback) {
 
-    var emailContent = '尊敬的慕课网用户，您好! <br> <br> <br>您在访问思沃学校时点击了“忘记密码”链接，这是一封密码重置确认邮件。<br>' +
-        '您可以通过点击以下链接重置帐户密码: <br>' + '<a>' + linkAddress + '</a>' +
+    var emailContent = '尊敬的思沃学院用户，您好! <br> <br> <br>您在访问思沃学院官网时点击了"忘记密码"链接，这是一封密码重置确认邮件。<br>' +
+        '您可以通过点击以下链接重置帐户密码: <div> <a href=\"' + linkAddress + '\">' + linkAddress + '</a> </div>' +
         '<br> <br> 为保障您的帐号安全，请在24小时内点击该链接，您也可以将链接复制到浏览器地址栏访问。 若如果您并未尝试修改密码，请忽略本邮件，由此给您带来的不便请谅解。' +
         '<br> <br> 本邮件由系统自动发出，请勿直接回复！';
 
