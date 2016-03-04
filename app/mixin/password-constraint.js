5b0e847c683235b12b96d10ad0d792ed4b6478db
wengjiaojiao <419909058@qq.com>
2016-03-04 14:51:56 +0800

    APP:remove the newpassword constraint

--- app/mixin/password-constraint.js
+++ app/mixin/password-constraint.js
@@ -8,23 +8,6 @@ var constraint = {
       maximum: 16,
       message: '^请输入合法旧密码'
     }
-  },
-
-  newPassword: {
-    presence: {message: '^请输入新密码'},
-    length: {
-      minimum: 8,
-      maximum: 16,
-      message: '^请输入合法新密码'
-    }
-  },
-
-  confirmPassword: {
-    presence: {message: '^请确认新密码'},
-    equality: {
-      attribute: 'newPassword',
-      message: '^两次密码不匹配'
-    }
   }
 };
 
