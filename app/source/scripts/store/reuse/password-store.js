e56d0bf472393314aca394ac2382a9b1f8d6d04b
wengjiaojiao <419909058@qq.com>
2016-03-04 14:54:49 +0800

    APP:create and add function to triiger

--- app/source/scripts/store/reuse/password-store.js
+++ app/source/scripts/store/reuse/password-store.js
@@ -1,3 +1,20 @@
-/**
- * Created by Jessica on 3/4/16.
- */
+'use strict';
+
+var Reflux = require('reflux');
+var PasswordActions = require('../../actions/reuse/password-actions');
+
+
+var UserDetailStore = Reflux.createStore({
+  listenables: [PasswordActions],
+
+  onChangeNewPassword: function(passwordObj) {
+    this.trigger(passwordObj);
+  },
+
+  onGetPasswordError: function(passwordError) {
+    this.trigger(passwordError);
+  }
+});
+
+
+module.exports = UserDetailStore;
