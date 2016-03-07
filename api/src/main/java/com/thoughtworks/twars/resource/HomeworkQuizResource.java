bd28be7d50d8237c739349dc18ae66018b0020ee
purpletianjing <TianjingSun@outlook.com>
2016-03-07 11:14:54 +0800

    swagger: add swagger to homeworkQuiz

--- api/src/main/java/com/thoughtworks/twars/resource/HomeworkQuizResource.java
+++ api/src/main/java/com/thoughtworks/twars/resource/HomeworkQuizResource.java
@@ -2,6 +2,10 @@ package com.thoughtworks.twars.resource;
 
 import com.thoughtworks.twars.bean.HomeworkQuiz;
 import com.thoughtworks.twars.mapper.HomeworkQuizMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -14,14 +18,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Path("/homeworkQuizzes")
+@Api
 public class HomeworkQuizResource {
     @Inject
     private HomeworkQuizMapper homeworkQuizMapper;
 
     @GET
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "successful"),
+    @ApiResponse(code = 404, message = "not found")})
     @Path("/{param}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getOneHomeworkQuiz(@PathParam("param") int id) {
+    public Response getOneHomeworkQuiz(
+            @ApiParam(value = "homeworkQuizId", allowableValues = "int", required = true)
+            @PathParam("param") int id) {
         HomeworkQuiz homeworkQuiz = homeworkQuizMapper.findById(id);
 
         if (homeworkQuiz == null) {
