0aa543c2654e3a9baec0c37454ea1b7106c3069f
Danping YAO <anlihuer@outlook.com>
2016-03-07 09:22:11 +0800

    SQL:Modify test data

---
+++ api/src/test/resources/db/migration/V46_2__Add_test_data_for_password_retrieve_detail_table.sql
@@ -0,0 +1,4 @@
+TRUNCATE passwordRetrieveDetail;
+INSERT INTO passwordRetrieveDetail VALUES (null,'test@163.com','ce08446f20b3b3aa4bee97298a1392b7',1451581261);
+INSERT INTO passwordRetrieveDetail VALUES (null,'wjj@qq.com','e652621b9bd77a2ea4a4495ab03e3cc8',UNIX_TIMESTAMP());
+INSERT INTO passwordRetrieveDetail VALUES (null,'test2@qq.com',null,null);
