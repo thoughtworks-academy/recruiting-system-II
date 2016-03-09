3a0a644061e6988b31cad810d52842a30131d926
purpletianjing <TianjingSun@outlook.com>
2016-03-09 21:17:45 +0800

    Api: add insertGithubUser

---
+++ api/src/main/java/com/thoughtworks/twars/bean/GithubUser.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.twars.bean;
+
+public class GithubUser {
+    int id;
+    int githubId;
+    int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGithubId() {
+        return githubId;
+    }
+
+    public void setGithubId(int githubId) {
+        this.githubId = githubId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+}
---
+++ api/src/main/java/com/thoughtworks/twars/mapper/GithubUserMapper.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.twars.mapper;
+
+import com.thoughtworks.twars.bean.GithubUser;
+
+public interface GithubUserMapper {
+    int insertGithubUser(GithubUser githubUser);
+}
