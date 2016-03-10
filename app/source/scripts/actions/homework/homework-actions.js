c0e18a27eed7945fc9236da8149d42a59983f89d
Evan-Leee <EvanLee_@outlook.com>
2016-03-10 10:18:48 +0800

    App: add a method getRunningResult()

--- app/source/scripts/actions/homework/homework-actions.js
+++ app/source/scripts/actions/homework/homework-actions.js
@@ -5,6 +5,7 @@ var Reflux = require('reflux');
 var HomeworkActions = Reflux.createActions([
   'changeOrderId',
   'loadHomeworkList',
+  'getRunningResult',
   'submitUrl',
   'submited',
   'getBranches',
