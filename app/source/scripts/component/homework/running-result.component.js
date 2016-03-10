1ba14a0d87a774c08808c3b9c770b35afed98f6f
Evan-Leee <EvanLee_@outlook.com>
2016-03-10 09:04:04 +0800

    App: add the running-result-component

--- app/source/scripts/component/homework/running-result.component.js
+++ app/source/scripts/component/homework/running-result.component.js
@@ -1,12 +1,39 @@
 'use strict';
 
 var React = require('react');
-
+var Reflux = require('reflux');
+var RunningResultStore = require('../../store/homework/running-result-store');
 var RunningResult = React.createClass({
+
+  mixins: [Reflux.connect(RunningResultStore)],
+  getInitialState: function () {
+    return {
+      isSubmited: false,
+      resultText: '',
+      resultStatus: ''
+    }
+  },
+
   render() {
     return (
         <div>
-          运行结果
+          {
+              this.state.isCommited ?
+                  <div className="prompt">
+                    <strong>正在进行测试,请稍候 ... ...</strong>
+                  </div>
+                :
+                  <div className="default">
+                    <strong>快点开始答题吧!</strong>
+                  </div>
+          }
+          <div className="runningResult">
+            <label>运行结果为:</label>
+            <div className="content">
+              {this.state.resultText}
+            </div>
+          </div>
+
         </div>
     );
   }
