cbedaca2030b1dca2f4fa793b204719f74c711f0
simpletrain <zsy498761554@live.com>
2016-03-11 10:31:58 +0800

    APP:Better user experience

--- app/vendor/vendor.js
+++ app/vendor/vendor.js
@@ -10,8 +10,12 @@ function Vendor() {
 
 Vendor.prototype.getBranches = function (req, res) {
   var originUrl = req.query.url;
+
   var list = originUrl.split('/');
-  var apiUrl = 'https://api.github.com/repos/' + list[3] + '/' + list[4] + '/branches';
+  var user = list[3];
+  var repo = list[4].replace(/.git/i,'');
+  var apiUrl = 'https://api.github.com/repos/' + user + '/' + repo + '/branches';
+
   superAgent.get(apiUrl)
       .set('Content-Type', 'application/json')
       .set(githubUser.header, githubUser.value)
