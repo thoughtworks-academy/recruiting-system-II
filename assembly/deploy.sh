1b4390258fa73facad24b158a507a418cca4ecd9
linwenjun <linwenjun@163.com>
2016-03-12 17:53:06 +0800

    Assembly: add node-app

--- assembly/deploy.sh
+++ assembly/deploy.sh
@@ -2,8 +2,35 @@
 
 set -e
 
-docker-compose kill
-docker-compose rm -f
-docker-compose up -d
+#docker-compose kill
+#docker-compose rm -f
+#docker-compose up -d
+cd assembly
+docker-compose kill jetty-api
+cd -
 
-docker exec -it assembly_mysql_1 /tmp/create-db.sh
+mv ./api/gradle.properties ./api/gradle.properties.bak
+cp ./assembly/assemble/jetty-api/gradle.properties ./api/gradle.properties
+./gradlew flywayMigrate
+mv ./api/gradle.properties.bak ./api/gradle.properties
+
+
+mv ./api/src/main/resources/config.properties ./api/src/main/resources/config.properties.bak
+cp ./assembly/assemble/jetty-api/config.properties ./api/src/main/resources/config.properties
+./gradlew clean
+./gradlew war
+cp api/build/libs/api.war assembly/assemble/jetty-api
+mv ./api/src/main/resources/config.properties.bak ./api/src/main/resources/config.properties
+
+cd assembly
+docker-compose up -d
+cd -
+#rm -fr assembly/assemble/*
+#git archive --format=zip HEAD:api -o assembly/assemble/api.zip
+#cd assembly/assemble
+#unzip api.zip -d api/
+#cd -
+#
+#cd assembly/assemble/api
+#./gradlew assemble
+#cd -
--- assembly/mysql/create-db.sh
+++ assembly/mysql/create-db.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mysql -u root -pthoughtworks -e 'CREATE DATABASE `BronzeSword` IF NOT EXISTS CHARACTER SET utf8 COLLATE utf8_general_ci;'
+mysql -u root -pthoughtworks -e 'CREATE DATABASE IF NOT EXISTS `BronzeSword` CHARACTER SET utf8 COLLATE utf8_general_ci;'
