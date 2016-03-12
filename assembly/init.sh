ecb3578bd76a15abae36ace79e0f80e8b5361984
linwenjun <linwenjun@163.com>
2016-03-12 16:27:30 +0800

    Assemble: finish the mysql

--- assembly/init.sh
+++ assembly/init.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-set -e
-
-docker-compose kill
-docker-compose rm -f
-docker-compose up -d
-
 docker exec -it assembly_mysql_1 /tmp/create-db.sh
---
+++ assembly/mysql/create-db.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mysql -u root -pthoughtworks -e 'CREATE DATABASE `BronzeSword` IF NOT EXISTS CHARACTER SET utf8 COLLATE utf8_general_ci;'
