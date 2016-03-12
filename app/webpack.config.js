217b0621b79a61d017d3e0462b36f56ba353429a
sialvsic <sialvsic@outlook.com>
2016-03-12 11:32:25 +0800

    App: add a entry for homework-details.html

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -4,55 +4,56 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var webpack = require("webpack");
 
 module.exports = {
-    entry: {
-        "404": "./source/scripts/404.js",
-        "index": "./source/scripts/index.js",
-        "logic-puzzle": "./source/scripts/logic-puzzle",
-        "homework": "./source/scripts/homework.js",
-        "register": "./source/scripts/register.js",
-        "start": "./source/scripts/start.js",
-        "user-center": './source/scripts/user-center.js',
-        "dashboard":'./source/scripts/dashboard.js',
-        "password-retrieve":'./source/scripts/password-retrieve.js',
-        "password-reset": './source/scripts/password-reset.js'
-    },
-    output: {
-        path: __dirname + '/public/assets/',
-        filename: '[name].bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-                test: /\.less$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
-            },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&minetype=application/font-woff"
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url-loader?limit=10000&name=build/[name].[ext]'
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin("[name].bundle.css")
-    ],
-    devtool: '#inline-source-map'
-    //resolve: {
-    //    extensions: [".jpg", ".png"]
-    //}
-}
-;
+  entry: {
+    "404": "./source/scripts/404.js",
+    "index": "./source/scripts/index.js",
+    "logic-puzzle": "./source/scripts/logic-puzzle",
+    "homework": "./source/scripts/homework.js",
+    "register": "./source/scripts/register.js",
+    "start": "./source/scripts/start.js",
+    "user-center": './source/scripts/user-center.js',
+    "dashboard": './source/scripts/dashboard.js',
+    "password-retrieve": './source/scripts/password-retrieve.js',
+    "password-reset": './source/scripts/password-reset.js',
+    "homework-details": './source/scripts/homework-details.js'
+  },
+  output: {
+    path: __dirname + '/public/assets/',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader?limit=10000&name=build/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].bundle.css")
+  ],
+  devtool: '#inline-source-map'
+  //resolve: {
+  //    extensions: [".jpg", ".png"]
+  //}
+};
