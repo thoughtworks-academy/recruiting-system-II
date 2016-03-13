e97d84be737c2c859cfae2f5d8db2585bd7a8b03
linwenjun <linwenjun@163.com>
2016-03-13 10:07:25 +0800

    Assembly: refactory app deployment script

--- assembly/deploy-app.sh
+++ assembly/deploy-app.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# 准备代码
 cd app
 npm install
 ./node_modules/.bin/webpack
 cd -
 
-# 停用app server
-cd assembly
-docker-compose kill node-app
-cd -
+# 删除文件
+rm -fr assembly/assemble/node-app/*
 
+# 写入文件
 cp -r app/* assembly/assemble/node-app
 
-# 启动app server
-cd assembly
-docker-compose up -d
-cd -
+# 写入配置文件
+cp assembly/conf/app-config.yml assembly/assemble/node-app/config/config.yml
