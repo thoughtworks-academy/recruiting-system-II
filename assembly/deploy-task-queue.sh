ee09a7a7a17f3a07a41e0299c4a019bb1b199761
linwenjun <linwenjun@163.com>
2016-03-13 10:13:31 +0800

    Assembly: Refactory task-queue-deploy

--- assembly/deploy-task-queue.sh
+++ assembly/deploy-task-queue.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# 准备代码
 cd task-queue
 npm install
 cd -
 
-# 停用task-queue server
-# cd assembly
-# docker-compose kill task-queue
-# cd -
-
+# 删除文件
+rm -fr assembly/assemble/task-queue/*
 # 将文件拷贝到目标地址
 cp -r task-queue/* assembly/assemble/task-queue
 
 # 写入配置文件
-cp assembly/assemble/conf/task-queue-conf.yml assembly/assemble/task-queue/config.yml
+cp assembly/conf/task-queue-config.yml assembly/assemble/task-queue/config/config.yml
 
-# 启动app server
-# cd assembly
-# docker-compose up -d
-# cd -
