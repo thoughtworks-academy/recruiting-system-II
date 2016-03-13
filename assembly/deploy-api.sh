ffdc5b7da916398eea2337a40adfc4b46abfb7b2
linwenjun <linwenjun@163.com>
2016-03-13 10:03:51 +0800

    Assembly: refactory api deploy

--- assembly/deploy-api.sh
+++ assembly/deploy-api.sh
@@ -2,31 +2,50 @@
 
 set -e
 
-# 修改apiserver migrate配置项
-mv ./api/gradle.properties ./api/gradle.properties.bak
-cp ./assembly/assemble/jetty-api/gradle.properties ./api/gradle.properties
-# 数据库迁移
-./gradlew flywayMigrate
-# 恢复apiserver migrate配置项
-mv ./api/gradle.properties.bak ./api/gradle.properties
+rm -fr assembly/assemble/jetty-api/*
+cp -r api/* assembly/assemble/jetty-api
+cp assembly/conf/config.properties assembly/assemble/jetty-api/src/main/resources/config.properties
+cp assembly/conf/gradle.properties assembly/assemble/jetty-api/gradle.properties
 
-# 修改apiserver配置项
-mv ./api/src/main/resources/config.properties ./api/src/main/resources/config.properties.bak
-cp ./assembly/assemble/jetty-api/config.properties ./api/src/main/resources/config.properties
+cd assembly/assemble/jetty-api
 ./gradlew clean
 ./gradlew war
-
-# 停用api server
-cd assembly
-docker-compose kill jetty-api
+./gradlew flywayMigrate
 cd -
 
-# 将生成好的api war包放入指定目录
-cp api/build/libs/api.war assembly/assemble/jetty-api
-# 恢复配置项
-mv ./api/src/main/resources/config.properties.bak ./api/src/main/resources/config.properties
+mkdir -p assembly/assemble/tmp
 
-# 启动api server
-cd assembly
-docker-compose up -d
-cd -
+cp assembly/assemble/jetty-api/build/libs/jetty-api.war assembly/assemble/tmp
+rm -fr assembly/assemble/jetty-api/*
+cp assembly/assemble/tmp/jetty-api.war assembly/assemble/jetty-api
+
+rm -fr mkdir -p assembly/assemble/tmp
+
+## 修改apiserver migrate配置项
+#mv ./api/gradle.properties ./api/gradle.properties.bak
+#cp ./assembly/assemble/jetty-api/gradle.properties ./api/gradle.properties
+## 数据库迁移
+#./gradlew flywayMigrate
+## 恢复apiserver migrate配置项
+#mv ./api/gradle.properties.bak ./api/gradle.properties
+#
+## 修改apiserver配置项
+#mv ./api/src/main/resources/config.properties ./api/src/main/resources/config.properties.bak
+#cp ./assembly/assemble/jetty-api/config.properties ./api/src/main/resources/config.properties
+#./gradlew clean
+#./gradlew war
+#
+## 停用api server
+#cd assembly
+#docker-compose kill jetty-api
+#cd -
+#
+## 将生成好的api war包放入指定目录
+#cp api/build/libs/api.war assembly/assemble/jetty-api
+## 恢复配置项
+#mv ./api/src/main/resources/config.properties.bak ./api/src/main/resources/config.properties
+#
+## 启动api server
+#cd assembly
+#docker-compose up -d
+#cd -
