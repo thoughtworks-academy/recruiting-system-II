15ab622aa1aa85f3fa01ada06b763c8af09feb46
sialvsic <sialvsic@outlook.com>
2016-03-16 09:43:11 +0800

    App: delete /:user/csv route

--- app/routes/routers/user.js
+++ app/routes/routers/user.js
@@ -6,7 +6,6 @@ var router = express.Router();
 var UserController = require('../../controllers/user-controller');
 var userController = new UserController();
 
-router.get('/:userId/csv', userController.exportCsv);
 router.get('/:userId/homework-details', userController.exportHomeworkDetails);
 
 module.exports = router;
