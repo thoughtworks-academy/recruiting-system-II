f72843de8281b39a0e8258a82de25bf128f55f26
linwenjun <linwenjun@163.com>
2016-03-17 18:09:00 +0800

    fix deploy shell bug

--- assembly/deploy-api-from-ci.sh
+++ assembly/deploy-api-from-ci.sh
@@ -4,10 +4,11 @@ set -e
 WORKDIRECTORY=$1
 rm -fr $WORKDIRECTORY/assembly/assemble/jetty-api/*
 cp $WORKDIRECTORY/assembly/conf/config.properties $WORKDIRECTORY/api/src/main/resources/config.properties
-cp $WORKDIRECTORY/assembly/conf/gradle.properties api/gradle.properties
+cp $WORKDIRECTORY/assembly/conf/gradle.properties $WORKDIRECTORY/api/gradle.properties
 
 cd $WORKDIRECTORY/api
 gradle clean
 gradle war
+cd -
 
 cp $WORKDIRECTORY/api/build/libs/api.war $WORKDIRECTORY/assembly/assemble/jetty-api/jetty-api.war
