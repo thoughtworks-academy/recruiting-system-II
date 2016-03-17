c6519204e3f6cc7b479d9d13b5539de14ef6a49f
wengjiaojiao <419909058@qq.com>
2016-03-17 15:17:00 +0800

    APP:add dropdown in navigation

--- app/source/scripts/component/navigation/navigation.component.js
+++ app/source/scripts/component/navigation/navigation.component.js
@@ -7,9 +7,17 @@ var Navigation = React.createClass({
           <div className="brand">
             <a href="/"><strong>思沃特训营</strong></a>
           </div>
-          <div className="header-right">
-            <a href="dashboard.html">控制台</a>
-            <a href="/logout">退出</a>
+          <div className="dropdown header-right">
+            <a className="dropdown-style" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              我的账户
+              <span className="caret"></span>
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+              <li><a href="user-center.html">个人中心</a></li>
+              <li><a href="dashboard.html">控制台</a></li>
+              <li role="separator" className="divider"></li>
+              <li><a href="/logout">退出</a></li>
+            </ul>
           </div>
         </nav>
     );
