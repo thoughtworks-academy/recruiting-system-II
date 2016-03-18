07a9bbb138334b44d24e4ecdee1846949f2649ef
wengjiaojiao <419909058@qq.com>
2016-03-18 14:22:04 +0800

    APP:render the head right component

--- app/source/scripts/index.js
+++ app/source/scripts/index.js
@@ -13,13 +13,16 @@ var style = {
 };
 var showUnder = 600;
 
+var HeadRight = require('./component/index/head-right.component');
+
 ReactDom.render(
     <div>
+      <HeadRight />
       <ScrollToTop showUnder={showUnder} style={style}>
         <div id="scroll-button">
           <i className="fa fa-angle-double-up fa-3x"></i>
         </div>
       </ScrollToTop>
     </div>,
-    document.getElementById('scroll-to-top')
+    document.getElementById('head-right')
 );
