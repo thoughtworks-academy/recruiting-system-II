1aabaa4d5fbb788ee29b6d0ae331f406bfb66100
wengjiaojiao <419909058@qq.com>
2016-03-18 13:36:37 +0800

    APP:add navigation controller

--- app/controllers/navigation-controller.js
+++ app/controllers/navigation-controller.js
@@ -1,3 +1,40 @@
-/**
- * Created by Jessica on 3/18/16.
- */
+'use strict';
+var apiRequest = require('../services/api-request');
+var constant = require('../mixin/constant');
+
+
+function NavigationController() {
+
+}
+
+NavigationController.prototype.loadAccount = (req, res) => {
+  if (req.session.user) {
+    var userId = req.session.user.id;
+    var url = 'users/' + userId;
+    apiRequest.get(url, function (err, resp) {
+      if (resp === undefined) {
+        res.send({
+          status: constant.httpCode.INTERNAL_SERVER_ERROR
+        });
+      } else if (resp.status === constant.httpCode.OK) {
+        res.send({
+          status: constant.httpCode.OK,
+          account: resp.body.email
+        });
+      } else if (resp.status === constant.httpCode.NOT_FOUND) {
+        res.send({
+          status: constant.httpCode.NOT_FOUND
+        });
+      } else {
+        res.status(constant.httpCode.INTERNAL_SERVER_ERROR);
+        res.send({
+          status: constant.httpCode.INTERNAL_SERVER_ERROR
+        });
+      }
+    });
+  }else {
+    res.send({status: constant.httpCode.ACCEPTED});
+  }
+};
+
+module.exports = NavigationController;
