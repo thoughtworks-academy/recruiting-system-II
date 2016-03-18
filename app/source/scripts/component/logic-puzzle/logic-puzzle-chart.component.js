4cd66bf5e585f028538366488ae7541b964acd97
XTWang <meeting_love_lengyi@hotmail.com>
2016-03-18 14:22:17 +0800

    APP: add a magnifying glass in logic-puzzle

--- app/source/scripts/component/logic-puzzle/logic-puzzle-chart.component.js
+++ app/source/scripts/component/logic-puzzle/logic-puzzle-chart.component.js
@@ -3,6 +3,9 @@
 var Reflux = require('reflux');
 var LogicPuzzleStore = require('../../store/logic-puzzle/logic-puzzle-store');
 
+var $ = jQuery;
+require('../../libs/jquery.elevatezoom.js');
+
 var LogicPuzzleChart = React.createClass({
   mixins: [Reflux.connect(LogicPuzzleStore)],
 
@@ -14,11 +17,24 @@ var LogicPuzzleChart = React.createClass({
     };
   },
 
+  componentDidUpdate: function() {
+    if("" !== this.state.item.chartPath) {
+      $('#logic-puzzle-img')
+          .removeData('zoomImage')
+          .elevateZoom({
+            zoomWindowFadeIn: 500,
+            zoomWindowFadeOut: 500,
+            lensFadeIn: 500,
+            lensFadeOut: 500
+          });
+    }
+  },
+
   render: function () {
     return (
         <div className="left-chart">
           <a href={this.state.item.chartPath} data-lightbox="image">
-            <img src={this.state.item.chartPath} alt="逻辑题图片"/>
+            <img id="logic-puzzle-img" src={this.state.item.chartPath} data-zoom-image={this.state.item.chartPath} alt="逻辑题图片"/>
           </a>
         </div>
     );
