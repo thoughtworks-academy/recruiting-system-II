b2550e9ebd2d2fec193b618c221525db3ee8f88b
linwenjun <linwenjun@163.com>
2016-03-18 11:04:22 +0800

    use ./gradlew replace gradle

---
+++ assembly/deploy-api-from-ci.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+WORKDIRECTORY=$1
+rm -fr $WORKDIRECTORY/assembly/assemble/jetty-api/*
+cp $WORKDIRECTORY/assembly/conf/config.properties $WORKDIRECTORY/api/src/main/resources/config.properties
+cp $WORKDIRECTORY/assembly/conf/gradle.properties $WORKDIRECTORY/api/gradle.properties
+
+cd $WORKDIRECTORY/api
+./gradlew clean
+./gradlew war
+cd -
+
+cp $WORKDIRECTORY/api/build/libs/api.war $WORKDIRECTORY/assembly/assemble/jetty-api/jetty-api.war
