b911cc51ac44c85448785114eced4d129951d1b1
wengjiaojiao <419909058@qq.com>
2016-03-18 19:02:58 +0800

    APP:extract account component

--- app/source/scripts/component/navigation/navigation.component.js
+++ app/source/scripts/component/navigation/navigation.component.js
@@ -1,39 +1,13 @@
 'use strict';
-var NavigationActions = require('../../actions/navigation/navigation-actions');
-var NavigationStore = require('../../store/navigation/navigation-store');
-var Reflux = require('reflux');
 
 var Navigation = React.createClass({
-  mixins: [Reflux.connect(NavigationStore)],
-
-  getInitialState: function() {
-    return {
-      account: ''
-    };
-  },
-
-  componentDidMount: function () {
-    NavigationActions.loadAccount();
-  },
-
   render: function () {
     return (
         <nav>
           <div className="brand">
             <a href="/"><strong>思沃特训营</strong></a>
           </div>
-          <div className="dropdown header-right">
-            <a className="dropdown-style" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-              {this.state.account}
-              <span className="caret"></span>
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-              <li><a href="user-center.html">个人中心</a></li>
-              <li><a href="dashboard.html">控制台</a></li>
-              <li role="separator" className="divider"></li>
-              <li><a href="/logout">退出</a></li>
-            </ul>
-          </div>
+          {this.props.children}
         </nav>
     );
   }
