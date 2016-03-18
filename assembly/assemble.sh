f013149ef09795e49f4ec0126b0878c79ddd385b
linwenjun <linwenjun@163.com>
2016-03-18 19:41:35 +0800

    use zip compress file

--- assembly/assemble.sh
+++ assembly/assemble.sh
@@ -29,7 +29,7 @@ cp -r app/* assembly/assemble/node-app
 # 写入配置文件
 cp assembly/conf/app-config.yml assembly/assemble/node-app/config/config.yml
 
-tar -zvcf assembly/assemble/nodeapp.tar.gz assembly/assemble/node-app
+zip -r assembly/assemble/nodeapp.zip assembly/assemble/node-app
 
 # task queue package
 cd task-queue
@@ -44,4 +44,4 @@ cp -r task-queue/* assembly/assemble/task-queue
 # 写入配置文件
 cp assembly/conf/task-queue-config.yml assembly/assemble/task-queue/config/config.yml
 # 压缩
-tar -zvcf assembly/assemble/task-queue.tar.gz assembly/assemble/task-queue
+zip -r assembly/assemble/task-queue.zip assembly/assemble/task-queue
