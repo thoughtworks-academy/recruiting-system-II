cf15a62859f66456a333920370025d1d080a29db
wengjiaojiao <419909058@qq.com>
2016-03-18 10:45:58 +0800

    APP:rename the second tag in homework

--- app/source/scripts/component/homework/homework-content.component.js
+++ app/source/scripts/component/homework/homework-content.component.js
@@ -12,7 +12,7 @@ var HomeworkContent = React.createClass({
       <div className="content">
         <Tabs defaultActiveKey={0} animation={false} getShowStatus={true}>
           <Tab eventKey={0} title="题目说明">{this.props.children[0]}</Tab>
-          <Tab eventKey={1} title="提交说明">{this.props.children[1]}</Tab>
+          <Tab eventKey={1} title="提交作业">{this.props.children[1]}</Tab>
           <Tab eventKey={2} title="运行结果">{this.props.children[2]}</Tab>
         </Tabs>
       </div>
