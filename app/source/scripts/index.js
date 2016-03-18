b17000227ed0abee47b78a403516e02d4ef200bd
simpletrain <zsy498761554@live.com>
2016-03-18 14:04:42 +0800

    APP: Add scroll to top button in index

--- app/source/scripts/index.js
+++ app/source/scripts/index.js
@@ -1,2 +1,24 @@
 'use strict';
 require('../less/index.less');
+
+var ScrollToTop = require('react-scroll-up');
+var style = {
+  position: 'fixed',
+  bottom: 400,
+  right: 30,
+  cursor: 'pointer',
+  transitionDuration: '0.2s',
+  transitionTimingFunction: 'linear',
+  transitionDelay: '0s'
+};
+
+ReactDom.render(
+    <div>
+      <ScrollToTop showUnder={600} style={style}>
+        <div id="scroll-button">
+          <i className="fa fa-angle-double-up fa-3x"></i>
+        </div>
+      </ScrollToTop>
+    </div>,
+    document.getElementById('scroll-to-top')
+);
