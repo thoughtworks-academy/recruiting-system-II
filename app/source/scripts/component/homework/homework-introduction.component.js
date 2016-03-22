9757e19578080ec40c59668f27a082161fd82ca2
sialvsic <sialvsic@outlook.com>
2016-03-22 15:18:13 +0800

    App:modified markdown trans

--- app/source/scripts/component/homework/homework-introduction.component.js
+++ app/source/scripts/component/homework/homework-introduction.component.js
@@ -1,9 +1,18 @@
 'use strict';
 
 var Reflux = require('reflux');
-var markdown = require('markdown').markdown;
 var HomeworkIntroductionStore = require('../../store/homework/homework-introduction-store');
-
+var marked = require('marked');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 var HomeworkIntroduction = React.createClass({
   mixins: [Reflux.connect(HomeworkIntroductionStore)],
@@ -19,7 +28,7 @@ var HomeworkIntroduction = React.createClass({
     var desc = this.state.desc;
 
     function content() {
-      return {__html: markdown.toHTML(desc)};
+      return {__html: marked(desc)};
     }
 
     return (
@@ -28,7 +37,6 @@ var HomeworkIntroduction = React.createClass({
             <div id="introduction" dangerouslySetInnerHTML={content()}>
             </div>
             <div className={'templateRepo ' + (this.state.showRepo ? '' : ' hide')}>
-              <span>编程题模板库地址:</span><em>{this.state.templateRepo}</em>
             </div>
           </div>
         </div>
