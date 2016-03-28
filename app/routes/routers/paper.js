a5dc8dbaa3035e8ecca03cecbfc327103c00b2dd
sialvsic <sialvsic@outlook.com>
2016-03-28 17:48:53 +0800

    App: add new route for export csv

--- app/routes/routers/paper.js
+++ app/routes/routers/paper.js
@@ -6,6 +6,8 @@ var router = express.Router();
 var PaperController = require('../../controllers/paper-controller');
 var paperController = new PaperController();
 
-router.get('/:paperId/scoresheet', paperController.exportCsv);
+router.get('/:paperId/scoresheet', paperController.exportScoresheetCsvForPaper);
+router.get('/:paperId/user/:userId/homework-details', paperController.exportHomeworkDetailsCsvForUser);
+router.get('/:paperId/user/:userId/homeworkquiz/:homeworkquiz', paperController.exportScoresheetCsvForPaper);
 
 module.exports = router;
