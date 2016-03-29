b0ce90a19812542945a84fc1b34372489b0d16ce
XTWang <meeting_love_lengyi@hotmail.com>
2016-03-29 18:02:46 +0800

    Task: add mongo conn

--- task-queue/app.js
+++ task-queue/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var route = require('./routes/route');
 var yamlConfig = require('node-yaml-config');
 var config = yamlConfig.load('./config/config.yml');
+var mongoConn = require('./services/mongo-conn');
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -18,6 +19,7 @@ route.setRoutes(app);
 
 app.listen(config.port, () => {
   console.log('App listening at http://localhost:' + config.port);
+  mongoConn.start(config.database);
 });
 
 module.exports = app;
