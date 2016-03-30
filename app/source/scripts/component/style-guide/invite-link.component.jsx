5e14a1597c86a2cd434f65a6afe6dab97120331d
wengjiaojiao <419909058@qq.com>
2016-03-30 15:34:33 +0800

    APP:use state to change the content

--- app/source/scripts/component/style-guide/invite-link.component.jsx
+++ app/source/scripts/component/style-guide/invite-link.component.jsx
@@ -1,11 +1,16 @@
 'use strict';
 
 var InviteLink = React.createClass({
+  getInitialState(){
+    return {
+      inviteLink: 'https://github.com/wengjiaojiao'
+    }
+  },
   render() {
     return(
       <div className="invite-link">
         邀请链接:
-        <label><input className="form-control" type="text" readOnly/></label>
+        <label><input className="form-control" type="text" value={this.state.inviteLink} readOnly/></label>
         <button className="btn btn-sm btn-default">复制</button>
       </div>
     )
