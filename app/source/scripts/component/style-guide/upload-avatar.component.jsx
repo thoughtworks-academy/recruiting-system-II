01154a2fccc156175dba746e9d47d2af64eb8821
wengjiaojiao <419909058@qq.com>
2016-03-30 16:29:07 +0800

    APP:change the state to choose the head protrait

--- app/source/scripts/component/style-guide/upload-avatar.component.jsx
+++ app/source/scripts/component/style-guide/upload-avatar.component.jsx
@@ -1,12 +1,16 @@
 'use strict';
 
 var UploadAvatar = React.createClass({
-
+  getInitialState() {
+    return {
+      headPortrait: require("../../../images/group-default.png")
+    }
+  },
   render (){
     return (
         <div className="upload-avatar col-md-12 col-sm-12 col-xs-12">
           <div className="avatar">
-            <img src={require("../../../images/group-default.png")} />
+            <img src={this.state.headPortrait} />
           </div>
           <div className="upload">
           <span><a href="#">
