fce39d2b8fbded9a165a007fb3b683d9cfd5c0b3
purpletianjing <TianjingSun@outlook.com>
2016-03-30 15:42:36 +0800

    App: group-avatar getInitialState

--- app/source/scripts/component/style-guide/group-avatar.component.jsx
+++ app/source/scripts/component/style-guide/group-avatar.component.jsx
@@ -1,20 +1,45 @@
 'use strict';
 
 var GroupAvatar = React.createClass({
+
+  getInitialState: function() {
+    return ({
+      groupList: [
+        {
+          groupName: '前端学习群',
+          groupAvatar: require("../../../images/1.pic_hd.jpg")
+        },
+        {
+          groupName: '数据库学习群',
+          groupAvatar: require("../../../images/group-default.png")
+        }
+      ]
+    })
+  },
+
   render () {
+    var groupList = this.state.groupList.map((item, index) => {
+      return(
+        <div className="col-md-3 col-sm-4 col-xs-6 text-center" key={index}>
+          <div className="avatar"><a href="#">
+            <img src={item.groupAvatar} />
+          </a></div>
+          <div><a href="#">
+            {item.groupName}
+          </a></div>
+        </div>
+      );
+    });
     return (
         <div>
+          {groupList}
           <div className="col-md-3 col-sm-4 col-xs-6 text-center">
-            <div className="avatar">
-              <img src={require("../../../images/1.pic_hd.jpg")} />
-            </div>
-            <div>很长的群组名称</div>
-          </div>
-          <div className="col-md-3 col-sm-4 col-xs-6 text-center">
-            <div className="avatar">
+            <div className="avatar"><a href="#">
               <span><i className="fa fa-plus text-success" /></span>
-            </div>
-            <div>群组名称 (要有字符数量限制)</div>
+            </a></div>
+            <div><a href="#">
+              添加群组
+            </a></div>
           </div>
         </div>
     );
