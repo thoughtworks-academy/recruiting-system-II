f8a11de17eac22941618f317d6d016054dd6e10a
wengjiaojiao <419909058@qq.com>
2016-03-30 13:39:10 +0800

    APP:extract add paper from style-guide to component

--- app/source/scripts/style-guide.jsx
+++ app/source/scripts/style-guide.jsx
@@ -5,11 +5,13 @@ var ListGroup = require('./component/style-guide/list-group.component.jsx');
 var GroupEvent = require('./component/style-guide/group-event.component.jsx');
 var DiscussFrame = require('./component/style-guide/discuss-frame.component.jsx');
 var Arrows = require('./component/style-guide/arrows.component.jsx');
+var AddPaper = require('./component/style-guide/add-paper.component.jsx');
 
 require('../less/group-title.less');
 require('../less/list-group.less');
 require('../less/group-event.less');
 require('../less/discuss-frame.less');
+require('../less/add-paper.less');
 
 ReactDom.render(
   <div>
@@ -18,6 +20,7 @@ ReactDom.render(
     <GroupEvent />
     <DiscussFrame />
     <Arrows />
+    <AddPaper />
   </div>,
   document.getElementById('style-guide')
 );
