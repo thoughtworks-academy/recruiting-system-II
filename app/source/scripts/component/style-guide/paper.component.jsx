0bf543662ebb6c6e322339a16dd8ca52f589bed1
wengjiaojiao <419909058@qq.com>
2016-03-30 15:20:17 +0800

    APP:add role to choose the show content

--- app/source/scripts/component/style-guide/paper.component.jsx
+++ app/source/scripts/component/style-guide/paper.component.jsx
@@ -3,22 +3,24 @@
 var Paper = React.createClass({
   getInitialState(){
     return {
+      paperName:'PaperName',
       isMarked: false,
       isPublished: false,
       sectionNumber: 10,
-      publishedNumber:1
+      publishedNumber:1,
+      role: 0
     }
   },
   render: function() {
     return (
         <div className="paper-button col-xs-12">
           <h3 className="col-xs-9">
-            name
+            {this.state.paperName}
           </h3>
           <div className="col-xs-3">
             <i className={"fa fa-2x" + (this.state.isMarkd ? ' fa-star' : ' fa-star-o')}></i>
           </div>
-          <div className="col-md-9 col-sm-5 ">
+          <div className={"col-md-9 col-sm-5" + (this.state.role === 1 ? '' : ' hide')}>
             <div className={this.state.isPublished ? 'hide' : ''}>未发布：
               <a href="#">点击发布</a>
             </div>
@@ -28,8 +30,8 @@ var Paper = React.createClass({
             <div>已发布个数：{this.state.publishedNumber}</div>
           </div>
           <div className="button-buttom">
-            <a href="#" className="text-warning"><b>编辑</b></a>
-            <a href="#" className="text-info"><b>导出成绩</b></a>
+            <a href="#" className={"text-warning" + (this.state.role === 1 ? '' : ' hide')}><b>编辑</b></a>
+            <a href="#" className={"text-info" + (this.state.role === 1 ? '' : ' hide')}><b>导出成绩</b></a>
             <a href="#" className="text-success"><b>开始答题</b></a>
           </div>
         </div>
