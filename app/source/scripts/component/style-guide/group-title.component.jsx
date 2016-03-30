b7ddf6b8be43e6db60ddf32c90276ec332b2348f
purpletianjing <TianjingSun@outlook.com>
2016-03-30 14:45:10 +0800

    App: add initialState

--- app/source/scripts/component/style-guide/group-title.component.jsx
+++ app/source/scripts/component/style-guide/group-title.component.jsx
@@ -2,10 +2,15 @@
 
 var GroupTitle = React.createClass({
 
+  getInitialState: function() {
+    return ({
+      titleName: '群组名称'
+    })
+  },
   render() {
     return (
       <div className="group-title">
-        <h4>群组名称</h4>
+        <h4>{this.state.titleName}</h4>
         <hr/>
       </div>
     );
