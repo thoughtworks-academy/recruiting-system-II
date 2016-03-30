b4e4d484cba0a4c53439dac453e8acc9eed6cbc0
wengjiaojiao <419909058@qq.com>
2016-03-30 19:20:38 +0800

    APP:modify the discuss frame css

--- app/source/scripts/component/style-guide/discuss-frame.component.jsx
+++ app/source/scripts/component/style-guide/discuss-frame.component.jsx
@@ -1,31 +1,35 @@
 'use strict';
 
 var DiscussFrame = React.createClass({
+  getInitialState: function () {
+    return {
+      isContent: true
+    }
+  },
+  changeState: function (mark) {
+    if(mark === 'content') {
+      this.setState({isContent: true});
+    }else {
+      this.setState({isContent: false});
+    }
+  },
+
   render: function() {
     return (
       <form className="form-horizontal col-md-12 col-sm-12 col-xs-12" >
         <div className="discuss-part">
           主题:<input className="form-control" type="text" placeholder="请输入主题"/>
         </div>
-        <div className="discuss-part">
-          内容:
-          <div className="bs-example bs-example-tabs" data-example-id="togglable-tabs">
-              <ul id="myTabs" className="nav nav-tabs" role="tablist">
-                <li role="presentation" className="active"><a href="#content" id="content-tab" role="tab" data-toggle="tab" aria-controls="content" aria-expanded="true">内容</a></li>
-                <li role="presentation"><a href="#preview" role="tab" id="preview-tab" data-toggle="tab" aria-controls="preview">预览</a></li>
-              </ul>
-              <div id="myTabContent" className="tab-content discuss-content">
-                <div role="tabpanel" className="tab-pane fade in active" id="content" aria-labelledby="content-tab">
-                  内容
-                </div>
-                <div role="tabpanel" className="tab-pane fade" id="preview" aria-labelledby="preview-tab">
-                  预览
-                </div>
-              </div>
-            </div>
+        <div className="discuss-type">
+          <div className={"discuss-item" + (this.state.isContent ? ' selected' : '')} onClick={this.changeState.bind(null, 'content')}>内容</div>
+          <div className={"discuss-item" + (this.state.isContent ? '' : ' selected')} onClick={this.changeState.bind(null, 'preview')}>预览</div>
         </div>
-        <div className="discuss-button">
-          <button type="button" className="btn btn-default">发布</button>
+        <div className="discuss-frame">
+          <span className={"discuss-arrow" + (this.state.isContent ? '' : ' preview')}></span>
+          <div className="discuss-textarea"></div>
+          <div className="discuss-button">
+            <input type="button" value="发布"/>
+          </div>
         </div>
       </form>
     )
