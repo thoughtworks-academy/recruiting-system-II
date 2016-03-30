fd2ee4e8c8f8dccd105e9f7c631221341b896b77
purpletianjing <TianjingSun@outlook.com>
2016-03-30 14:58:49 +0800

    App: list-group getInitialState

--- app/source/scripts/component/style-guide/list-group.component.jsx
+++ app/source/scripts/component/style-guide/list-group.component.jsx
@@ -2,35 +2,36 @@
 
 var ListGroup = React.createClass({
 
+  getInitialState: function() {
+    return ({
+      title: '个人中心',
+      list: ['群组首页','群组试卷','群组成员','群组管理'],
+      clickNumber: 1
+    })
+  },
+
   render() {
+
+    var listContent = this.state.list.map((item, index) => {
+      var classStr = "list-group-item " + (this.state.clickNumber === index + 1 ? 'select': '');
+      return (
+        <a className={classStr} href="javascript:void(0)" key={index}>
+          <div className="row">
+            <div className="h4 text-center">{item}</div>
+          </div>
+        </a>
+      )
+    });
+
     return (
       <div>
         <div className="list-group">
           <div className="list-group-item active">
             <div className="row">
-              <div className="h4 text-center">个人中心</div>
+              <div className="h4 text-center">{this.state.title}</div>
             </div>
           </div>
-          <a className="list-group-item select" href="javascript:void(0)">
-            <div className="row">
-              <div className="h4 text-center">群组首页</div>
-            </div>
-          </a>
-          <a className="list-group-item" href="javascript:void(0)">
-            <div className="row">
-              <div className="h4 text-center">群组试卷</div>
-            </div>
-          </a>
-          <a className="list-group-item" href="javascript:void(0)">
-            <div className="row">
-              <div className="h4 text-center">群组成员</div>
-            </div>
-          </a>
-          <a className="list-group-item" href="javascript:void(0)">
-            <div className="row">
-              <div className="h4 text-center">群组管理</div>
-            </div>
-          </a>
+          {listContent}
         </div>
       </div>
     );
