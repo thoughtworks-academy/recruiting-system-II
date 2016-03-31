dc98ce1d534f0a4a613eab900fb4ffbbb2a9d92b
Evan-Leee <EvanLee_@outlook.com>
2016-03-31 09:35:50 +0800

    App: delete the useless class

--- app/source/scripts/component/style-guide/invite-link.component.jsx
+++ app/source/scripts/component/style-guide/invite-link.component.jsx
@@ -9,7 +9,7 @@ var InviteLink = React.createClass({
   render() {
     return (
       <div className="invite-link col-md-12 col-sm-12 col-xs-12">
-        <p className="col-md-2 col-sm-2 col-xs-4 text-right invite-label">邀请链接:</p>
+        <p className="col-md-2 col-sm-2 col-xs-4 text-right">邀请链接:</p>
         <div className="col-md-4 col-sm-6 col-xs-8">
           <div className="input-group">
             <input type="text" className="form-control" value={this.state.inviteLink} readOnly />
