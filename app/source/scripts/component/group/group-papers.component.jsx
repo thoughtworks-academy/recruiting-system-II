80170e5cb1f75ca0a95d0fe84ed19ea9d62212f3
Evan-Leee <EvanLee_@outlook.com>
2016-04-01 13:20:17 +0800

    App: add the group-papers component

---
+++ app/source/scripts/component/group/group-papers.component.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+var GroupTitle = require("../style-guide/group-title.component.jsx");
+
+var GroupPapers = React.createClass({
+
+  render() {
+    return (
+      <GroupTitle titleName="我关注的试卷"/>
+    );
+  }
+});
+
+module.exports = GroupPapers;
