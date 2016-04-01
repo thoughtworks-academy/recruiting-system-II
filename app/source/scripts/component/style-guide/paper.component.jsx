60c7813e68b687e78b2103cca1f5a5c06f95ce11
Evan-Leee <EvanLee_@outlook.com>
2016-04-01 17:06:50 +0800

    App: write the group-paper page

--- app/source/scripts/component/style-guide/paper.component.jsx
+++ app/source/scripts/component/style-guide/paper.component.jsx
@@ -3,12 +3,13 @@
 var Paper = React.createClass({
   getInitialState(){
     return {
-      paperName:'PaperNamePaperName',
-      isMarked: false,
-      isPublished: false,
-      sectionNumber: 10,
-      publishedNumber:1,
-      role: 0
+      paperName: this.props.item.paperName || 'PaperNamePaperName',
+      isMarked: this.props.item.isMarked,
+      isPublished: this.props.item.isPublished,
+      sectionNumber: this.props.item.sectionNumber || 10 ,
+      publishedNumber: this.props.item.publishedNumber || 1,
+      role:  this.props.item.role || '2',
+      isFinished: this.props.item.isFinished
     }
   },
   render: function() {
@@ -18,9 +19,9 @@ var Paper = React.createClass({
             {this.state.paperName}
           </h3>
           <div className="col-xs-3">
-            <i className={"fa fa-2x" + (this.state.isMarkd ? ' fa-star' : ' fa-star-o')}></i>
+            <i className={"fa fa-2x" + (this.state.isMarked ? ' fa-star' : ' fa-star-o')} />
           </div>
-          <div className={"col-md-9 col-sm-5" + (this.state.role === 1 ? '' : ' hide')}>
+          <div className={"col-md-9 col-sm-5" + (this.state.role === '1' ? '' : ' hide')}>
             <div className={this.state.isPublished ? 'hide' : ''}>未发布：
               <a href="#">点击发布</a>
             </div>
@@ -29,10 +30,10 @@ var Paper = React.createClass({
             <div>章节个数：{this.state.sectionNumber}</div>
             <div>已发布个数：{this.state.publishedNumber}</div>
           </div>
-          <div className="button-buttom">
-            <a href="#" className={"text-warning" + (this.state.role === 1 ? '' : ' hide')}><b>编辑</b></a>
-            <a href="#" className={"text-info" + (this.state.role === 1 ? '' : ' hide')}><b>导出成绩</b></a>
-            <a href="#" className="text-success"><b>开始答题</b></a>
+          <div className="button-bottom">
+            <a href="#" className={"text-warning" + (this.state.role === '1' ? '' : ' unvisible')}><b>编辑</b></a>
+            <a href="#" className={"text-info" + (this.state.role === '1' ? '' : ' unvisible')}><b>导出成绩</b></a>
+            <a href="#" className={"text-success" + (this.state.isFinished ? ' unvisible' : '')}><b>开始答题</b></a>
           </div>
         </div>
     )
