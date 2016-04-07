d2a0cd68495c98fd1dec88ea8d1c667b5e70941b
Evan-Leee <EvanLee_@outlook.com>
2016-04-07 14:52:02 +0800

    add restful router

--- app.js
+++ app.js
@@ -10,10 +10,35 @@ app.use(express.static('web/public/assets'));
 
 app.use('/api', proxy('192.168.99.100:3000/',{
   forwardPath: function(req, res) {
-
     return require('url').parse(req.url).path;
   }
 }));
+
+app.get('/index', function (req, res) {
+  res.sendFile(path.join(__dirname, 'web/public/assets', 'index.html'));
+});
+
+app.get('/register', function (req, res) {
+  res.sendFile(path.join(__dirname, 'web/public/assets', 'register.html'));
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, 'web/public/assets', 'register.html'));
+});
+
+//-------group开发阶段路由方法--------
+app.get('/group/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'web/public/assets', 'group.html'));
+});
+
+app.get('/group', function (req, res) {
+  res.sendFile(path.join(__dirname, 'web/public/assets', 'group.html'));
+});
+//-------group开发阶段路由方法--------
+
+router.get('/styleGuide',function(req, res){
+  res.sendFile(path.join(__dirname, 'web/public/assets', 'style-guide.html'));
+});
 
 app.get('*', function (req, res){
   res.sendFile(path.join(__dirname, 'web/public/assets/', '404.html'));
